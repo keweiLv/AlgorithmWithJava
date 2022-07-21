84dfb05acd113e18b832193126f952fdd64af623
kezi <wow.kimsohyun.wu@gamil.com>
2022-07-21 23:52:09 +0800

    0721 daily

--- ByteDance/src/swordFingerProvided/Solution.java
+++ ByteDance/src/swordFingerProvided/Solution.java
@@ -415,4 +415,19 @@ public class Solution {
 		}
 		return ret;
 	}
+
+	// 和大于等于target鞥最短子数组
+	public int minSubArrayLen(int target, int[] nums) {
+		int left = 0;
+		int total = 0;
+		int ret = Integer.MAX_VALUE;
+		for (int right = 0; right < nums.length; right++) {
+			total += nums[right];
+			while (total >= target) {
+				ret = Math.min(ret, right - left + 1);
+				total -= nums[left++];
+			}
+		}
+		return ret > nums.length ? 0 : ret;
+	}
 }
