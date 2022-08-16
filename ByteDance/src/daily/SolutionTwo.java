d4a878044d021895809e62aae654c5f6f88422a9
kezi <wow.kimsohyun.wu@gmail.com>
2022-08-16 23:36:45 +0800

    0816 daily

--- ByteDance/src/daily/SolutionTwo.java
+++ ByteDance/src/daily/SolutionTwo.java
@@ -225,4 +225,42 @@ public class SolutionTwo {
         }
         return x == rev || x == rev / 10;
     }
+
+    // 一年中的第几天
+    static int[] nums = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    static int[] f = new int[13];
+    static {
+        for (int i = 1; i <= 12; i++) {
+            f[i] = f[i - 1] + nums[i - 1];
+        }
+    }
+    public int dayOfYear(String date) {
+        String[] ss = date.split("-");
+        int y = Integer.parseInt(ss[0]), m = Integer.parseInt(ss[1]), d = Integer.parseInt(ss[2]);
+        boolean isLeap = (y % 4 == 0 && y % 100 != 0) || y % 400 == 0;
+        int ans = m > 2 && isLeap ? f[m - 1] + 1 : f[m - 1];
+        return ans + d;
+    }
+
+    // 含有k个元素的组合
+    List<List<Integer>> ans;
+    public List<List<Integer>> combine(int n,int k){
+        ans = new ArrayList<>();
+        dfs(n,1,k,new ArrayList<>());
+        return ans;
+    }
+    private void dfs(int n, int i, int k, List<Integer> list) {
+        // 剪枝
+        if (n - i + 1 < k){
+            return;
+        }
+        if (k == 0){
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+        list.add(i);
+        dfs(n,i+1,k-1,list);
+        list.remove(list.size()-1);
+        dfs(n,i+1,k,list);
+    }
 }
