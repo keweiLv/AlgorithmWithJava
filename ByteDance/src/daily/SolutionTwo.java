c2431e55355b88023287350c193f99362a63a0ea
kezi <wow.kimsohyun.wu@gamil.com>
2022-09-18 23:17:58 +0800

    0918 daily

--- ByteDance/src/daily/SolutionTwo.java
+++ ByteDance/src/daily/SolutionTwo.java
@@ -531,17 +531,51 @@ public class SolutionTwo {
 			}
 			idx[i] = j;
 		}
-		for (int i = n-1;i>=0;i--){
-			if (!list.get(idx[i]).equals(list.get(i))){
+		for (int i = n - 1; i >= 0; i--) {
+			if (!list.get(idx[i]).equals(list.get(i))) {
 				int c = list.get(idx[i]);
-				list.set(idx[i],list.get(i));
-				list.set(i,c);
+				list.set(idx[i], list.get(i));
+				list.set(i, c);
 				break;
 			}
 		}
-		for (int i = n-1;i>=0;i--){
+		for (int i = n - 1; i >= 0; i--) {
 			ans = ans * 10 + list.get(i);
 		}
 		return ans;
 	}
-}
+
+	// 盛最多水的容器
+	public int maxArea(int[] height) {
+		int i = 0, j = height.length - 1, res = 0;
+		while (i < j) {
+			res = height[i] < height[j] ? Math.max(res, (j - i) * height[i++]) : Math.max(res, (j - 1) * height[j--]);
+		}
+		return res;
+	}
+
+	// 从前序遍历与中序遍历序列构造二叉树
+	private Map<Integer, Integer> indexMap;
+
+	public TreeNode buildTree(int[] preorder, int[] inorder) {
+		int n = preorder.length;
+		indexMap = new HashMap<>();
+		for (int i = 0; i < n; i++) {
+			indexMap.put(inorder[i], i);
+		}
+		return myBuildTree(preorder, inorder, 0, n - 1, 0, n - 1);
+	}
+
+	public TreeNode myBuildTree(int[] preorder, int[] inorder, int preorder_left, int preorder_right, int inorder_left, int inorder_right) {
+		if (preorder_left > preorder_right) {
+			return null;
+		}
+		int preorder_root = preorder_left;
+		int inorder_root = indexMap.get(preorder[preorder_root]);
+		TreeNode root = new TreeNode(preorder[preorder_root]);
+		int size_left_subtree = inorder_root - inorder_left;
+		root.left = myBuildTree(preorder, inorder, preorder_left + 1, preorder_left + size_left_subtree, inorder_left, inorder_root - 1);
+		root.right = myBuildTree(preorder, inorder, preorder_left + size_left_subtree + 1, preorder_right, inorder_root + 1, inorder_right);
+		return root;
+	}
+} 
