23433cf1885a46705d89a9946833a6b3332442cb
kezi <wow.kimsohyun.wu@gamil.com>
2022-09-25 23:18:09 +0800

    0925 daily

--- ByteDance/src/LC_StudyPlan75_Lv3/Solution.java
+++ ByteDance/src/LC_StudyPlan75_Lv3/Solution.java
@@ -295,5 +295,20 @@ public class Solution {
 		}
 		return dp[len];
 	}
+
+	// 子集
+	public List<List<Integer>> subsets(int[] nums) {
+		List<List<Integer>> res = new ArrayList<>();
+		backtrace(0,nums,res,new ArrayList<>());
+		return res;
+	}
+	private void backtrace(int i,int[] nums,List<List<Integer>> res,ArrayList<Integer> tmp){
+		res.add(new ArrayList<>(tmp));
+		for (int j = i;j<nums.length;j++){
+			tmp.add(nums[j]);
+			backtrace(j+1,nums,res,tmp);
+			tmp.remove(tmp.size()-1);
+		}
+	}
 }
 
--- ByteDance/src/daily/SolutionTwo.java
+++ ByteDance/src/daily/SolutionTwo.java
@@ -687,4 +687,26 @@ public class SolutionTwo {
 		}
 		return ans;
 	}
+
+	// 旋转数字
+	public int rotatedDigits(int n) {
+		int ans = 0;
+		out:for (int i = 1;i<=n;i++){
+			boolean ok = false;
+			int x = i;
+			while (x != 0){
+				int t = x % 10;
+				x /= 10;
+				if (t == 2 || t ==5 || t == 6 || t == 9){
+					ok = true;
+				}else if (t != 0 && t != 1 && t != 8){
+					continue out;
+				}
+			}
+			if (ok){
+				ans++;
+			}
+		}
+		return ans;
+	}
 } 
