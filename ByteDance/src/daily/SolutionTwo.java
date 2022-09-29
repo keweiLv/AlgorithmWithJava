1de06dced49c43f4d3509901d7e30811b258163d
kezi <wow.kimsohyun.wu@gamil.com>
2022-09-29 23:45:23 +0800

    0929 daily

--- ByteDance/src/daily/SolutionTwo.java
+++ ByteDance/src/daily/SolutionTwo.java
@@ -769,4 +769,9 @@ public class SolutionTwo {
 		}
 		return ans[k];
 	}
+
+	// 字符串轮转
+	public boolean isFlipedString(String s1, String s2) {
+		return s1.length() == s2.length() && (s1 + s1).contains(s2);
+	}
 } 
