19e9816fb76bb68e20dd5049253dc4aa786dbe22
kezi <wow.kimsohyun.wu@gamil.com>
2022-09-30 00:13:23 +0800

    0929 daily

--- ByteDance/src/LC_StudyPlan75_Lv3/Solution.java
+++ ByteDance/src/LC_StudyPlan75_Lv3/Solution.java
@@ -310,5 +310,20 @@ public class Solution {
 			tmp.remove(tmp.size()-1);
 		}
 	}
+
+	// 寻找旋转排序数组中的最小值
+	public int findMin(int[] nums) {
+		int left = 0;
+		int right = nums.length-1;
+		while (left < right){
+			int mid = left + (right - left)/2;
+			if (nums[mid] > nums[right]){
+				left = mid + 1;
+			}else if (nums[mid] < nums[right]){
+				right = mid;
+			}
+		}
+		return nums[left];
+	}
 }
 
