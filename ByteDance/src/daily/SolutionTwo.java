2abdce0e14314b705de73e3972070119ee4a065d
kezi <wow.kimsohyun.wu@gamil.com>
2022-10-03 23:16:35 +0800

    1003 daily

--- ByteDance/src/daily/SolutionTwo.java
+++ ByteDance/src/daily/SolutionTwo.java
@@ -779,4 +779,14 @@ public class SolutionTwo {
 	public boolean checkOnesSegment(String s) {
 		return !s.contains("01");
 	}
+
+	// 一维数组的动态和
+	public int[] runningSum(int[] nums) {
+		int n = nums.length;
+		for (int i = 1; i < n; i++) {
+			nums[i] = nums[i-1] + nums[i];
+		}
+		return nums;
+	}
+
 } 
