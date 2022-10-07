7282ec65d5d2efa27010ad0b26c492db3f8a3527
kezi <wow.kimsohyun.wu@gamil.com>
2022-10-07 22:50:49 +0800

    1007 daily

--- ByteDance/src/daily/SolutionTwo.java
+++ ByteDance/src/daily/SolutionTwo.java
@@ -843,4 +843,18 @@ public class SolutionTwo {
 		return s;
 	}
 
+
+	// 最大升序子数组和
+	public int maxAscendingSum(int[] nums) {
+		int n = nums.length,ans = nums[0];
+		for (int i = 1,cur = nums[0]; i < n; i++) {
+			if (nums[i] > nums[i-1]){
+				cur += nums[i];
+			}else {
+				cur = nums[i];
+			}
+			ans = Math.max(ans,cur);
+		}
+		return ans;
+	}
 } 
