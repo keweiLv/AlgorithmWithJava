cb2e41d119335f018e9d2848429122186866d338
kezi <wow.kimsohyun.wu@gamil.com>
2022-10-14 00:09:35 +0800

    1013 daily

--- ByteDance/src/forjob/Solution.java
+++ ByteDance/src/forjob/Solution.java
@@ -90,4 +90,20 @@ public class Solution {
 		}
 		return res;
 	}
+
+	// 最多能完成排序的块
+	public int maxChunksToSorted(int[] arr) {
+		int n = arr.length, ans = 0;
+		for (int i = 0, j = 0, min = n, max = -1; i < n; i++) {
+			min = Math.min(min, arr[i]);
+			max = Math.max(max, arr[i]);
+			if (j == min && i == max) {
+				ans++;
+				j = i + 1;
+				min = n;
+				max = -1;
+			}
+		}
+		return ans;
+	}
 }
