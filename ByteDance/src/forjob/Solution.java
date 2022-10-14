6aa5c2af6e36bcbc8efd92ed664372099548b6c4
kezi <wow.kimsohyun.wu@gamil.com>
2022-10-14 23:38:57 +0800

    1014 daily

--- ByteDance/src/forjob/Solution.java
+++ ByteDance/src/forjob/Solution.java
@@ -106,4 +106,31 @@ public class Solution {
 		}
 		return ans;
 	}
+
+	// 全排列
+	public List<List<Integer>> permute(int[] nums) {
+		List<List<Integer>> res = new ArrayList<>();
+		int len = nums.length;
+		boolean[] used = new boolean[len];
+		Deque<Integer> path = new ArrayDeque<>(len);
+		dfs(nums,len,0,path,used,res);
+		return res;
+	}
+
+	private void dfs(int[] nums, int len, int depth, Deque<Integer> path, boolean[] used, List<List<Integer>> res) {
+		if (depth == len){
+			res.add(new ArrayList<>(path));
+			return;
+		}
+		for (int i = 0;i<len;i++){
+			if (!used[i]){
+				path.addLast(nums[i]);
+				used[i] = true;
+				dfs(nums,len,depth+1,path,used,res);
+				used[i] = false;
+				path.removeLast();
+			}
+		}
+	}
+
 }
