f03793f6bde99ad7a531ad9eaf9daee399cb4abe
kezi <wow.kimsohyun.wu@gamil.com>
2022-10-21 23:57:42 +0800

    1021 daily

--- ByteDance/src/forjob/Solution.java
+++ ByteDance/src/forjob/Solution.java
@@ -276,4 +276,33 @@ public class Solution {
 		dfs(root.left, u << 1, depth + 1);
 		dfs(root.right, u << 1 | 1, depth + 1);
 	}
+
+	// 一次编辑
+	public boolean oneEditAway(String a, String b) {
+		int n = a.length(), m = b.length();
+		if (Math.abs(n - m) > 1) {
+			return false;
+		}
+		if (n > m) {
+			return oneEditAway(b, a);
+		}
+		int i = 0, j = 0, cnt = 0;
+		while (i < n && j < m && cnt <= 1) {
+			char c1 = a.charAt(i), c2 = b.charAt(j);
+			if (c1 == c2) {
+				i++;
+				j++;
+			} else {
+				if (n == m) {
+					i++;
+					j++;
+					cnt++;
+				} else {
+					j++;
+					cnt++;
+				}
+			}
+		}
+		return cnt <= 1;
+	}
 }
