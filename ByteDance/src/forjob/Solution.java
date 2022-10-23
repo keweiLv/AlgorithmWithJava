0814c2ad08081cd7fc87090415f9d4d9e75610e1
kezi <wow.kimsohyun.wu@gamil.com>
2022-10-23 22:58:58 +0800

    1023 daily

--- ByteDance/src/forjob/Solution.java
+++ ByteDance/src/forjob/Solution.java
@@ -305,4 +305,19 @@ public class Solution {
 		}
 		return cnt <= 1;
 	}
+
+	// 交替合并字符串
+	public String mergeAlternately(String s1, String s2) {
+		int n = s1.length(), m = s2.length(), i = 0, j = 0;
+		StringBuilder sb = new StringBuilder();
+		while (i < n || j < m) {
+			if (i < n){
+				sb.append(s1.charAt(i++));
+			}
+			if (j < m){
+				sb.append(s2.charAt(j++));
+			}
+		}
+		return sb.toString();
+	}
 }
