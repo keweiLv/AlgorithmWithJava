be852b3a295b71435048873c3e50f8783dfbb76a
kezi <wow.kimsohyun.wu@gamil.com>
2022-10-24 23:29:50 +0800

    1024 daily

--- ByteDance/src/forjob/Solution.java
+++ ByteDance/src/forjob/Solution.java
@@ -340,4 +340,34 @@ public class Solution {
 		}
 		return ans.next;
 	}
+
+	// 分割数组
+	public int partitionDisjoint(int[] nums) {
+		int n = nums.length;
+		int[] min = new int[n + 10];
+		min[n - 1] = nums[n - 1];
+		for (int i = n - 2; i >= 0; i--) {
+			min[i] = Math.min(min[i + 1], nums[i]);
+		}
+		for (int i = 0, max = 0; i < n - 1; i++) {
+			max = Math.max(max, nums[i]);
+			if (max <= min[i + 1]) {
+				return i + 1;
+			}
+		}
+		return -1;
+	}
+
+	// 将数字翻译成字符串]
+	public int translateNum(int num) {
+		String s = String.valueOf(num);
+		int a = 1, b = 1;
+		for (int i = 2; i <= s.length(); i++) {
+			String tmp = s.substring(i - 2, i);
+			int c = tmp.compareTo("10") >= 0 && tmp.compareTo("25") <= 0 ? a + b : a;
+			b = a;
+			a = c;
+		}
+		return a;
+	}
 }
