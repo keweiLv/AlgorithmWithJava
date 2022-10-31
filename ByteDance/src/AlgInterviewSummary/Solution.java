132d886a40402a93273f0b6608f48a3cc766af66
kezi <wow.kimsohyun.wu@gamil.com>
2022-10-31 22:28:25 +0800

    1031 daily

--- ByteDance/src/AlgInterviewSummary/Solution.java
+++ ByteDance/src/AlgInterviewSummary/Solution.java
@@ -1,6 +1,8 @@
 package AlgInterviewSummary;
 
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -31,7 +33,7 @@ public class Solution {
 		int j = n - 1;
 		int end = m + n - 1;
 		while (j >= 0) {
-			nums1[end--] = (i >= 0 && nums1[i] > nums2[j])?nums1[i--]:nums2[j--];
+			nums1[end--] = (i >= 0 && nums1[i] > nums2[j]) ? nums1[i--] : nums2[j--];
 		}
 	}
 
@@ -39,20 +41,71 @@ public class Solution {
 	public List<String> letterCasePermutation(String S) {
 		List<String> res = new ArrayList<>();
 		char[] chars = S.toCharArray();
-		dfs(chars,0,res);
+		dfs(chars, 0, res);
 		return res;
 	}
 
 	private void dfs(char[] chars, int i, List<String> res) {
-		if (i == chars.length){
+		if (i == chars.length) {
 			res.add(new String(chars));
 			return;
 		}
-		dfs(chars,i+1,res);
-		if (Character.isLetter(chars[i])){
+		dfs(chars, i + 1, res);
+		if (Character.isLetter(chars[i])) {
 			chars[i] ^= 1 << 5;
-			dfs(chars,i+1,res);
+			dfs(chars, i + 1, res);
 		}
 	}
 
+	// 神奇字符串
+	public int magicalString(int n) {
+		char[] s = new char[n + 2];
+		s[0] = 1;
+		s[1] = s[2] = 2;
+		char c = 2;
+		for (int i = 2, j = 3; j < n; i++) {
+			// 1^3 = 2,2^3 = 1
+			c ^= 3;
+			s[j++] = c;
+			if (s[i] == 2) {
+				s[j++] = c;
+			}
+		}
+		int ans = 0;
+		for (int i = 0; i < n; i++) {
+			ans += 2 - s[i];
+		}
+		return ans;
+	}
+
+	// 滑动窗口的最大值
+	public int[] maxSlidingWindow(int[] nums, int k) {
+		int n = nums.length;
+		Deque<Integer> deque = new LinkedList<>();
+		for (int i = 0; i < k; i++) {
+			while (!deque.isEmpty() && nums[i] >= nums[deque.peekLast()]) {
+				deque.pollLast();
+			}
+			deque.offerLast(i);
+		}
+		int[] ans = new int[n - k + 1];
+		ans[0] = nums[deque.peekFirst()];
+		for (int i = k; i < n; ++i) {
+			while (!deque.isEmpty() && nums[i] >= nums[deque.peekLast()]) {
+				deque.pollLast();
+			}
+			deque.offerLast(i);
+			while (deque.peekFirst() <= i -k){
+				deque.pollFirst();
+			}
+			ans[i-k+1]= nums[deque.peekFirst()];
+		}
+		return ans;
+	}
+
+	// 删除中间节点
+	public void deleteNode(ListNode node) {
+		node.val = node.next.val;
+		node.next = node.next.next;
+	}
 }
