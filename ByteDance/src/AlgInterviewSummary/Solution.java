05f21408b17c73e2e57af73413305a9852c777de
kezi <wow.kimsohyun.wu@gamil.com>
2022-11-04 00:15:26 +0800

    1103 daily

--- ByteDance/src/AlgInterviewSummary/Solution.java
+++ ByteDance/src/AlgInterviewSummary/Solution.java
@@ -136,4 +136,20 @@ public class Solution {
 		}
 		return true;
 	}
+
+	// 最大重复子字符串
+	public int maxRepeating(String sequence, String word) {
+		int n = sequence.length(),m = word.length(),ans = 0;
+		int[] f = new int[n+10];
+		for (int i = 1;i<=n;i++){
+			if (i - m < 0){
+				continue;
+			}
+			if (sequence.substring(i-m,i).equals(word)){
+				f[i] = f[i-m] + 1;
+			}
+			ans = Math.max(ans,f[i]);
+		}
+		return ans;
+	}
 }
