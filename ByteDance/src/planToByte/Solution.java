5290104067a5f6406e7f125d2ae040b03f0dc32c
kezi <wow.kimsohyun.wu@gamil.com>
2022-11-08 23:36:07 +0800

    1108 daily

--- ByteDance/src/planToByte/Solution.java
+++ ByteDance/src/planToByte/Solution.java
@@ -1,5 +1,10 @@
 package planToByte;
 
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
 /**
  * @author Kezi
  * @date 2022年11月07日 22:54
@@ -10,7 +15,7 @@ public class Solution {
 	public ListNode reverseList(ListNode head) {
 		ListNode pre = null;
 		ListNode cur = head;
-		while (cur!=null){
+		while (cur != null) {
 			ListNode next = cur.next;
 			cur.next = pre;
 			pre = next;
@@ -23,13 +28,46 @@ public class Solution {
 	public int maxProfit(int prices[]) {
 		int min = Integer.MAX_VALUE;
 		int max = 0;
-		for (int i = 0;i<prices.length;i++){
-			if (prices[i] < min){
+		for (int i = 0; i < prices.length; i++) {
+			if (prices[i] < min) {
 				min = prices[i];
-			}else if (prices[i] - min > max){
-				max = prices[i]- min;
+			} else if (prices[i] - min > max) {
+				max = prices[i] - min;
 			}
 		}
 		return max;
 	}
+
+	/**
+	 * 无重复字符的最长子串
+	 */
+	public int lengthOfLongestSubstring(String s) {
+		Map<Character, Integer> map = new HashMap<>();
+		int max = 0;
+		int left = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (map.containsKey(s.charAt(i))) {
+				left = Math.max(left, map.get(s.charAt(i)) + 1);
+			}
+			map.put(s.charAt(i), i);
+			max = Math.max(max, i - left + 1);
+		}
+		return max;
+	}
+
+	// 数组中第K大的元素
+	public int findKthLargest(int[] nums, int k) {
+		PriorityQueue<Integer> queue = new PriorityQueue<>(k, Comparator.comparing(a -> a));
+		for (int i = 0; i < k; i++) {
+			queue.offer(nums[i]);
+		}
+		for (int i = k; i < nums.length; i++) {
+			if (nums[i] > queue.peek()){
+				queue.poll();
+				queue.offer(nums[i]);
+			}
+		}
+		return queue.peek();
+	}
+
 }
