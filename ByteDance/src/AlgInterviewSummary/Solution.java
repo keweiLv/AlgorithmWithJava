01a38594795429bd1e69dd577ca7a1d04cd1890d
kezi <wow.kimsohyun.wu@gmail.com>
2022-11-08 19:40:11 +0800

    1108 daily

--- ByteDance/src/AlgInterviewSummary/Solution.java
+++ ByteDance/src/AlgInterviewSummary/Solution.java
@@ -1,9 +1,6 @@
 package AlgInterviewSummary;
 
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author Kezi
@@ -235,5 +232,24 @@ public class Solution {
             }
         }
         return list;
+    }
+
+    // 统计一致字符串的数目
+    public int countConsistentStrings(String allowed, String[] words) {
+        boolean[] hash = new boolean[26];
+        for (char c : allowed.toCharArray()) {
+            hash[c - 'a'] = true;
+        }
+        int ans = 0;
+        out:
+        for (String s : words) {
+            for (char c : s.toCharArray()) {
+                if (!hash[c - 'a']) {
+                    continue out;
+                }
+            }
+            ans++;
+        }
+        return ans;
     }
 }
