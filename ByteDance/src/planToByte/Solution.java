9638033b8e1f9561137f8eef9ee2afc6dde7f4f3
kezi <wow.kimsohyun.wu@gamil.com>
2022-11-13 23:09:16 +0800

    1113 daily

--- ByteDance/src/planToByte/Solution.java
+++ ByteDance/src/planToByte/Solution.java
@@ -164,4 +164,24 @@ public class Solution {
 		return count == 0;
 	}
 
+	// 自定义字符串排序
+	public String customSortString(String order, String s) {
+		int[] cnts = new int[26];
+		for (char c : s.toCharArray()) {
+			cnts[c - 'a']++;
+		}
+		StringBuilder sb = new StringBuilder();
+		for (char c : order.toCharArray()) {
+			while (cnts[c - 'a']-- > 0) {
+				sb.append(c);
+			}
+		}
+		for (int i = 0; i < 26; i++) {
+			while (cnts[i]-- > 0) {
+				sb.append((char) (i + 'a'));
+			}
+		}
+		return sb.toString();
+	}
+
 }
