5282aab49cb4acdb9acac8f913b8cd7dba09bd65
kezi <wow.kimsohyun.wu@gamil.com>
2022-11-18 23:43:54 +0800

    1118 daily

--- ByteDance/src/planToByte/Solution.java
+++ ByteDance/src/planToByte/Solution.java
@@ -262,4 +262,21 @@ public class Solution {
 		dfs(root.left,depth);
 	}
 
+	// 翻转单词顺序
+	public String reverseWords(String s) {
+		s = s.trim();
+		int j = s.length()-1,i= j;
+		StringBuilder res = new StringBuilder();
+		while (i>=0){
+			while (i >= 0 && s.charAt(i) != ' '){
+				i--;
+			}
+			res.append(s.substring(i+1,j+1)+" ");
+			while (i >= 0 && s.charAt(i) == ' '){
+				i--;
+			}
+			j = i;
+		}
+		return res.toString().trim();
+	}
 }
