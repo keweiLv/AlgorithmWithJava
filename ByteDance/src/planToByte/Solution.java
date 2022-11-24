bd9f05379aba3eab2d89981664f9fc94ca46c4bb
kezi <wow.kimsohyun.wu@gamil.com>
2022-11-24 23:16:59 +0800

    1124 daily

--- ByteDance/src/planToByte/Solution.java
+++ ByteDance/src/planToByte/Solution.java
@@ -461,4 +461,13 @@ public class Solution {
 			dfs(index+1,num,digit);
 		}
 	}
+
+	// 圆圈中最后剩下的数字
+	public int lastRemaining(int n, int m) {
+		int ans = 0;
+		for (int i = 2;i<=n;i++){
+			ans = (ans + m) % i;
+		}
+		return ans;
+	}
 }
