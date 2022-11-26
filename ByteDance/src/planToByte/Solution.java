fc97a8de17d8f6f9427f4de55e9352e2d5153049
kezi <wow.kimsohyun.wu@gamil.com>
2022-11-26 22:55:41 +0800

    1126 daily

--- ByteDance/src/planToByte/Solution.java
+++ ByteDance/src/planToByte/Solution.java
@@ -521,4 +521,23 @@ public class Solution {
 		}
 		return p1 == sc.length && p2 == wtc.length;
 	}
+
+	// 删除有序数组中的重复项
+	public int removeDuplicates(int[] nums) {
+		if (nums == null || nums.length == 0) {
+			return 0;
+		}
+		int p = 0, q = 1;
+		while (q < nums.length) {
+			if (nums[p] != nums[q]) {
+				if (q - p > 1) {
+					nums[p + 1] = nums[q];
+				}
+				p++;
+			}
+			q++;
+		}
+		return p + 1;
+	}
+
 }
