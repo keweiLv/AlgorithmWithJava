f6c37a003401182dce3282eea93c70861fd1112c
kezi <wow.kimsohyun.wu@gamil.com>
2022-12-05 23:09:56 +0800

    1205 daily

--- ByteDance/src/planToByte/Solution.java
+++ ByteDance/src/planToByte/Solution.java
@@ -849,4 +849,29 @@ public class Solution {
 		return res;
 	}
 
+
+	// 串联所有单词的子串
+	public List<Integer> findSubstring(String s, String[] words) {
+		int n = s.length(),m = words.length,w = words[0].length();
+		Map<String,Integer> map = new HashMap<>();
+		for (String word:words){
+			map.put(word,map.getOrDefault(word,0)+1);
+		}
+		List<Integer> ans = new ArrayList<>();
+		out:for (int i = 0;i+m*w <= n;i++){
+			Map<String,Integer> cur = new HashMap<>();
+			String sub = s.substring(i,i+m*w);
+			for (int j = 0;j < sub.length();j+=w){
+				String item = sub.substring(j,j+w);
+				if (!map.containsKey(item)){
+					continue out;
+				}
+				cur.put(item,cur.getOrDefault(item,0)+1);
+			}
+			if (cur.equals(map)){
+				ans.add(i);
+			}
+		}
+		return ans;
+	}
 }
