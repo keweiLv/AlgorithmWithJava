c58548270cb1cc7b6ca075ca8daeecba215997b4
kezi <wow.kimsohyun.wu@gamil.com>
2022-12-09 23:57:18 +0800

    1209 daily

--- ByteDance/src/planToByte/Solution.java
+++ ByteDance/src/planToByte/Solution.java
@@ -1053,4 +1053,15 @@ public class Solution {
 		}
 		return false;
 	}
+
+	// 判断一个数字是否可以表示成三的幂的和
+	public boolean checkPowersOfThree(int n) {
+		while (n != 0){
+			if (n % 3 == 2){
+				return false;
+			}
+			n /= 3;
+		}
+		return true;
+	}
 }
