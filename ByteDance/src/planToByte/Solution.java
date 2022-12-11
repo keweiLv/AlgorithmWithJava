fe7b42578a1b021134638e93c75a97352dcdadc5
kezi <wow.kimsohyun.wu@gamil.com>
2022-12-11 22:09:23 +0800

    1211 daily

--- ByteDance/src/planToByte/Solution.java
+++ ByteDance/src/planToByte/Solution.java
@@ -1117,4 +1117,38 @@ public class Solution {
 		return ans;
 	}
 
+	// 最少操作使数组递增
+	public int minOperations(int[] nums) {
+		int ans = 0, max = 0;
+		for (int num : nums) {
+			ans += Math.max(0, max + 1 - num);
+			max = Math.max(max + 1, num);
+		}
+		return ans;
+	}
+
+	// 三个数的最大乘积
+	public int maximumProduct(int[] nums) {
+		int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+		int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
+		for (int num : nums) {
+			if (num < min1) {
+				min2 = min1;
+				min1 = num;
+			} else if (num < min2) {
+				min2 = num;
+			}
+			if (num > max1) {
+				max3 = max2;
+				max2 = max1;
+				max1 = num;
+			} else if (num > max2) {
+				max3 = max2;
+				max2 = num;
+			} else if (num > max3) {
+				max3 = num;
+			}
+		}
+		return Math.max(min1 * min2 * max1, max1 * max2 * max3);
+	}
 }
