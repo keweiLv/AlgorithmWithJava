5d5ca6d9deef4d7fecf86483a4becf39f894acbf
kezi <wow.kimsohyun.wu@gamil.com>
2022-12-25 22:40:52 +0800

    1225 daily

--- ByteDance/src/planToByte/AnotherSolution.java
+++ ByteDance/src/planToByte/AnotherSolution.java
@@ -169,4 +169,21 @@ public class AnotherSolution {
 		return sb.toString();
 	}
 
+	// 放置盒子
+	public int minimumBoxes(int n) {
+		int s = 0, k = 1;
+		while (s + k * (k + 1) / 2 <= n) {
+			s += k * (k + 1) / 2;
+			++k;
+		}
+		--k;
+		int ans = k * (k + 1) / 2;
+		k = 1;
+		while (s < n) {
+			++ans;
+			s += k;
+			++k;
+		}
+		return ans;
+	}
 }
