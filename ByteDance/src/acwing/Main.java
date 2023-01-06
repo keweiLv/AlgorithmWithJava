17c3a0853ba99f742731dd8e34529e8f4619cf27
kezi <wow.kimsohyun.wu@gmail.com>
2023-01-06 22:54:49 +0800

    2023-0106 daily

--- ByteDance/src/acwing/Main.java
+++ ByteDance/src/acwing/Main.java
@@ -1,12 +1,21 @@
 package acwing;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * @author Kezi
  * @date 2023年01月03日 23:38
  */
 public class Main {
+
+
+    static int N = 100010;
+    static int[] a = new int[N];
+    static int[] b = new int[N];
+
+
 //	public static void main(String[] args) {
 //		Scanner sc = new Scanner(System.in);
 //		int n = sc.nextInt();
@@ -21,31 +30,30 @@ public class Main {
 //	}
 
 
-	private static void quickSort(int[] q, int l, int r) {
-		if (l >= r) {
-			return;
-		}
-		int x = q[l + r >> 1], i = l - 1, j = r + 1;
-		while (i < j) {
-			while (q[++i] < x) ;
-			while (q[--j] > x) ;
-			if (i < j) {
-				int t = q[i];
-				q[i] = q[j];
-				q[j] = t;
-			}
-		}
-		quickSort(q, l, j);
-		quickSort(q, j + 1, r);
-	}
+    private static void quickSort(int[] q, int l, int r) {
+        if (l >= r) {
+            return;
+        }
+        int x = q[l + r >> 1], i = l - 1, j = r + 1;
+        while (i < j) {
+            while (q[++i] < x) ;
+            while (q[--j] > x) ;
+            if (i < j) {
+                int t = q[i];
+                q[i] = q[j];
+                q[j] = t;
+            }
+        }
+        quickSort(q, l, j);
+        quickSort(q, j + 1, r);
+    }
 
 
-	// 第K个数
-	static int N = 100010;
-	static int[] A = new int[N];
-	static int n, k;
+    // 第K个数
+    static int[] A = new int[N];
+    static int n, k;
 
-	//	public static void main(String[] args) {
+    //	public static void main(String[] args) {
 //		Scanner sc = new Scanner(System.in);
 //		n = sc.nextInt();
 //		k = sc.nextInt();
@@ -54,28 +62,28 @@ public class Main {
 //		}
 //		System.out.print(quickSortForK(0,n-1,k-1));
 //	}
-	public static int quickSortForK(int l, int r, int k) {
-		if (l >= r) {
-			return A[k];
-		}
-		int x = A[l], i = l - 1, j = r + 1;
-		while (i < j) {
-			do i++; while (A[i] < x);
-			do j--; while (A[j] > x);
-			if (i < j) {
-				int temp = A[i];
-				A[i] = A[j];
-				A[j] = temp;
-			}
-		}
-		if (k <= j) {
-			return quickSortForK(l, j, k);
-		} else {
-			return quickSortForK(j + 1, r, k);
-		}
-	}
+    public static int quickSortForK(int l, int r, int k) {
+        if (l >= r) {
+            return A[k];
+        }
+        int x = A[l], i = l - 1, j = r + 1;
+        while (i < j) {
+            do i++; while (A[i] < x);
+            do j--; while (A[j] > x);
+            if (i < j) {
+                int temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
+            }
+        }
+        if (k <= j) {
+            return quickSortForK(l, j, k);
+        } else {
+            return quickSortForK(j + 1, r, k);
+        }
+    }
 
-	// 高精度加法
+    // 高精度加法
 //	public static void main(String[] args) throws IOException {
 //		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 //		BigInteger a = new BigInteger(reader.readLine());
@@ -84,7 +92,7 @@ public class Main {
 //		reader.close();
 //	}
 
-	// 前缀和
+    // 前缀和
 //	public static void main(String[] args) {
 //		Scanner sc = new Scanner(System.in);
 //		int n = sc.nextInt();
@@ -105,18 +113,69 @@ public class Main {
 //		}
 //	}
 
-	// 二进制中1的个数
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		while (n-- != 0) {
-			int x = sc.nextInt();
-			int res = 0;
-			while (x != 0){
-				x &= (x-1);
-				res++;
-			}
-			System.out.print(res + " ");
-		}
-	}
+    // 二进制中1的个数
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		int n = sc.nextInt();
+//		while (n-- != 0) {
+//			int x = sc.nextInt();
+//			int res = 0;
+//			while (x != 0){
+//				x &= (x-1);
+//				res++;
+//			}
+//			System.out.print(res + " ");
+//		}
+//	}
+
+//    // 最长连续不重复子序列
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        int n = Integer.parseInt(reader.readLine());
+//        int[] nums = new int[n];
+//        String[] str = reader.readLine().split(" ");
+//        for (int i = 0; i < n; i++) {
+//            nums[i] = Integer.parseInt(str[i]);
+//        }
+//        Map<Integer, Integer> map = new HashMap<>();
+//        int res = 0;
+//        for (int i = 0, j = 0; i < n; i++) {
+//            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+//            while (map.get(nums[i]) > 1) {
+//                map.put(nums[j], map.get(nums[j]) - 1);
+//                j++;
+//            }
+//            res = Math.max(res,i-j+1);
+//        }
+//        System.out.println(res);
+//    }
+
+    // 数组元素的目标和
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] strings = reader.readLine().split(" ");
+        int n = Integer.parseInt(strings[0]);
+        int m = Integer.parseInt(strings[1]);
+        int x = Integer.parseInt(strings[2]);
+
+        String[] A = reader.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(A[i]);
+        }
+        String[] B = reader.readLine().split(" ");
+        for (int i = 0; i < m; i++) {
+            b[i] = Integer.parseInt(B[i]);
+        }
+        int i = 0, j = m - 1;
+        while (i < n && j >= 0) {
+            if (a[i] + b[j] > x){
+                j--;
+            }else if (a[i] + b[j] < x){
+                i++;
+            }else {
+                System.out.println(i + " " + j);
+                break;
+            }
+        }
+    }
 }
