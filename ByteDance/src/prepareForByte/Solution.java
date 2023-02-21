df7a552c277aac85dfda8df1c340ef2094de16ad
kezi <wow.kimsohyun.wu@gamil.com>
2023-02-21 23:54:03 +0800

    2023-0221 daily

--- ByteDance/src/prepareForByte/Solution.java
+++ ByteDance/src/prepareForByte/Solution.java
@@ -97,4 +97,46 @@ public class Solution {
 		}
 		return fast;
 	}
+
+	// 排序数组中只出现一次的数字
+	public int singleNonDuplicate(int[] nums) {
+		int n = nums.length, l = 0, r = nums.length - 1;
+		int ans = -1;
+		while (l <= r) {
+			int mid = l + (r - l) / 2;
+			if (mid < n - 1 && nums[mid] == nums[mid + 1]) {
+				if (mid % 2 == 0) {
+					l = mid + 2;
+				} else {
+					r = mid - 1;
+				}
+			} else if (mid > 0 && nums[mid] == nums[mid - 1]) {
+				if (mid % 2 == 0) {
+					r = mid - 2;
+				} else {
+					l = mid + 1;
+				}
+			} else {
+				ans = nums[mid];
+				break;
+			}
+		}
+		return ans;
+	}
+
+	// 子集
+	public List<List<Integer>> subsets(int[] nums) {
+		List<List<Integer>> res = new ArrayList<>();
+		backtrack(0, nums, res, new ArrayList<>());
+		return res;
+	}
+
+	private void backtrack(int i, int[] nums, List<List<Integer>> res, ArrayList<Integer> tmp) {
+		res.add(new ArrayList<>(tmp));
+		for (int j = i; j < nums.length; j++) {
+			tmp.add(nums[j]);
+			backtrack(j + 1, nums, res, tmp);
+			tmp.remove(tmp.size() - 1);
+		}
+	}
 }
