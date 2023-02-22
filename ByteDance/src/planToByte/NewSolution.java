79b206020d1f5d12e6db4395b1202195df04068b
kezi <wow.kimsohyun.wu@gamil.com>
2023-02-23 00:31:10 +0800

    2023-0222 daily

--- ByteDance/src/planToByte/NewSolution.java
+++ ByteDance/src/planToByte/NewSolution.java
@@ -223,4 +223,23 @@ public class NewSolution {
 		}
 		return pair ? "Pair" : "High Card";
 	}
+
+	// 石子游戏Ⅱ
+	public int stoneGameII(int[] piles) {
+		int n = piles.length, sum = 0;
+		int[][] dp = new int[n][n + 1];
+		for (int i = n - 1; i >= 0; i--) {
+			sum += piles[i];
+			for (int M = 1; M <= n; M++) {
+				if (i + 2 * M >= n) {
+					dp[i][M] = sum;
+				} else {
+					for (int x = 1; x <= 2 * M; x++) {
+						dp[i][M] = Math.max(dp[i][M], sum - dp[i + x][Math.max(M, x)]);
+					}
+				}
+			}
+		}
+		return dp[0][1];
+	}
 }
