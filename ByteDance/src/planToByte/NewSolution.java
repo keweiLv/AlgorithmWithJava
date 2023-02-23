5c034749579febcceca306fd96de17696dadc191
kezi <wow.kimsohyun.wu@gamil.com>
2023-02-24 00:17:23 +0800

    2023-0223 daily

--- ByteDance/src/planToByte/NewSolution.java
+++ ByteDance/src/planToByte/NewSolution.java
@@ -242,4 +242,13 @@ public class NewSolution {
 		}
 		return dp[0][1];
 	}
+
+	// 循环码排列
+	public List<Integer> circularPermutation(int n, int start) {
+		List<Integer> ans = new ArrayList<>();
+		for (int i = 0; i < 1 << n; i++) {
+			ans.add(i ^ (i >> 1) ^ start);
+		}
+		return ans;
+	}
 }
--- ByteDance/src/prepareForByte/Solution.java
+++ ByteDance/src/prepareForByte/Solution.java
@@ -139,4 +139,52 @@ public class Solution {
 			tmp.remove(tmp.size() - 1);
 		}
 	}
+
+	// 重排链表
+	public void reorderList(ListNode head) {
+		if (head == null) {
+			return;
+		}
+		ListNode mid = middleNode(head);
+		ListNode l1 = head;
+		ListNode l2 = mid.next;
+		mid.next = null;
+		l2 = reverseList(l2);
+		mergeList(l1, l2);
+	}
+
+	private void mergeList(ListNode l1, ListNode l2) {
+		ListNode l1_tmp;
+		ListNode l2_tmp;
+		while (l1 != null && l2 != null) {
+			l1_tmp = l1.next;
+			l2_tmp = l2.next;
+			l1.next = l2;
+			l1 = l1_tmp;
+			l2.next = l1;
+			l2 = l2_tmp;
+		}
+	}
+
+	private ListNode reverseList(ListNode head) {
+		ListNode prev = null;
+		ListNode curr = head;
+		while (curr != null) {
+			ListNode next = curr.next;
+			curr.next = prev;
+			prev = curr;
+			curr = next;
+		}
+		return prev;
+	}
+
+	private ListNode middleNode(ListNode head) {
+		ListNode slow = head;
+		ListNode fast = head;
+		while (fast.next != null && fast.next.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		return slow;
+	}
 }
