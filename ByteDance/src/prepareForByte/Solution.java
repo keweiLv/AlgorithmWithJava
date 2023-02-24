3f79589d4b179d7649f1c6b652b1ee6bacb06f14
kezi <wow.kimsohyun.wu@gmail.com>
2023-02-24 21:38:55 +0800

    2023-0224 daily

--- ByteDance/src/prepareForByte/Solution.java
+++ ByteDance/src/prepareForByte/Solution.java
@@ -9,182 +9,198 @@ import java.util.*;
 public class Solution {
 
 
-	// 买卖股票的最佳时机
-	public int maxProfit(int[] prices) {
-		int minPrices = Integer.MAX_VALUE;
-		int maxProfit = 0;
-		for (int i = 0; i < prices.length; i++) {
-			if (prices[i] < minPrices) {
-				minPrices = prices[i];
-			} else if (prices[i] - minPrices > maxProfit) {
-				maxProfit = prices[i] - minPrices;
-			}
-		}
-		return maxProfit;
-	}
+    // 买卖股票的最佳时机
+    public int maxProfit(int[] prices) {
+        int minPrices = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrices) {
+                minPrices = prices[i];
+            } else if (prices[i] - minPrices > maxProfit) {
+                maxProfit = prices[i] - minPrices;
+            }
+        }
+        return maxProfit;
+    }
 
-	// 打家劫舍Ⅱ
-	public int rob(int[] nums) {
-		if (nums.length == 0) {
-			return 0;
-		}
-		if (nums.length == 1) {
-			return nums[0];
-		}
-		return Math.max(myRob(Arrays.copyOfRange(nums, 0, nums.length - 1)), myRob(Arrays.copyOfRange(nums, 1, nums.length)));
-	}
+    // 打家劫舍Ⅱ
+    public int rob(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        return Math.max(myRob(Arrays.copyOfRange(nums, 0, nums.length - 1)), myRob(Arrays.copyOfRange(nums, 1, nums.length)));
+    }
 
-	private int myRob(int[] nums) {
-		int pre = 0, cur = 0, tmp;
-		for (int num : nums) {
-			tmp = cur;
-			cur = Math.max(pre + num, cur);
-			pre = tmp;
-		}
-		return cur;
-	}
+    private int myRob(int[] nums) {
+        int pre = 0, cur = 0, tmp;
+        for (int num : nums) {
+            tmp = cur;
+            cur = Math.max(pre + num, cur);
+            pre = tmp;
+        }
+        return cur;
+    }
 
-	// 字符串的排列
-	List<String> res = new LinkedList<>();
-	char[] c;
+    // 字符串的排列
+    List<String> res = new LinkedList<>();
+    char[] c;
 
-	public String[] permutation(String s) {
-		c = s.toCharArray();
-		dfs(0);
-		return res.toArray(new String[res.size()]);
-	}
+    public String[] permutation(String s) {
+        c = s.toCharArray();
+        dfs(0);
+        return res.toArray(new String[res.size()]);
+    }
 
-	private void dfs(int x) {
-		if (x == c.length - 1) {
-			res.add(String.valueOf(c));
-			return;
-		}
-		Set<Character> set = new HashSet<>();
-		for (int i = x; i < c.length; i++) {
-			if (set.contains(c[i])) {
-				continue;
-			}
-			set.add(c[i]);
-			swap(i, x);
-			dfs(x + 1);
-			swap(i, x);
-		}
-	}
+    private void dfs(int x) {
+        if (x == c.length - 1) {
+            res.add(String.valueOf(c));
+            return;
+        }
+        Set<Character> set = new HashSet<>();
+        for (int i = x; i < c.length; i++) {
+            if (set.contains(c[i])) {
+                continue;
+            }
+            set.add(c[i]);
+            swap(i, x);
+            dfs(x + 1);
+            swap(i, x);
+        }
+    }
 
-	private void swap(int i, int x) {
-		char tmp = c[i];
-		c[i] = c[x];
-		c[x] = tmp;
-	}
+    private void swap(int i, int x) {
+        char tmp = c[i];
+        c[i] = c[x];
+        c[x] = tmp;
+    }
 
-	// 环形链表
-	public ListNode detectCycle(ListNode head) {
-		ListNode fast = head, slow = head;
-		while (true) {
-			if (fast == null || fast.next == null) {
-				return null;
-			}
-			fast = fast.next.next;
-			slow = slow.next;
-			if (fast == slow) {
-				break;
-			}
-		}
-		fast = head;
-		while (fast != slow) {
-			fast = fast.next;
-			slow = slow.next;
-		}
-		return fast;
-	}
+    // 环形链表
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (true) {
+            if (fast == null || fast.next == null) {
+                return null;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                break;
+            }
+        }
+        fast = head;
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return fast;
+    }
 
-	// 排序数组中只出现一次的数字
-	public int singleNonDuplicate(int[] nums) {
-		int n = nums.length, l = 0, r = nums.length - 1;
-		int ans = -1;
-		while (l <= r) {
-			int mid = l + (r - l) / 2;
-			if (mid < n - 1 && nums[mid] == nums[mid + 1]) {
-				if (mid % 2 == 0) {
-					l = mid + 2;
-				} else {
-					r = mid - 1;
-				}
-			} else if (mid > 0 && nums[mid] == nums[mid - 1]) {
-				if (mid % 2 == 0) {
-					r = mid - 2;
-				} else {
-					l = mid + 1;
-				}
-			} else {
-				ans = nums[mid];
-				break;
-			}
-		}
-		return ans;
-	}
+    // 排序数组中只出现一次的数字
+    public int singleNonDuplicate(int[] nums) {
+        int n = nums.length, l = 0, r = nums.length - 1;
+        int ans = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (mid < n - 1 && nums[mid] == nums[mid + 1]) {
+                if (mid % 2 == 0) {
+                    l = mid + 2;
+                } else {
+                    r = mid - 1;
+                }
+            } else if (mid > 0 && nums[mid] == nums[mid - 1]) {
+                if (mid % 2 == 0) {
+                    r = mid - 2;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                ans = nums[mid];
+                break;
+            }
+        }
+        return ans;
+    }
 
-	// 子集
-	public List<List<Integer>> subsets(int[] nums) {
-		List<List<Integer>> res = new ArrayList<>();
-		backtrack(0, nums, res, new ArrayList<>());
-		return res;
-	}
+    // 子集
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(0, nums, res, new ArrayList<>());
+        return res;
+    }
 
-	private void backtrack(int i, int[] nums, List<List<Integer>> res, ArrayList<Integer> tmp) {
-		res.add(new ArrayList<>(tmp));
-		for (int j = i; j < nums.length; j++) {
-			tmp.add(nums[j]);
-			backtrack(j + 1, nums, res, tmp);
-			tmp.remove(tmp.size() - 1);
-		}
-	}
+    private void backtrack(int i, int[] nums, List<List<Integer>> res, ArrayList<Integer> tmp) {
+        res.add(new ArrayList<>(tmp));
+        for (int j = i; j < nums.length; j++) {
+            tmp.add(nums[j]);
+            backtrack(j + 1, nums, res, tmp);
+            tmp.remove(tmp.size() - 1);
+        }
+    }
 
-	// 重排链表
-	public void reorderList(ListNode head) {
-		if (head == null) {
-			return;
-		}
-		ListNode mid = middleNode(head);
-		ListNode l1 = head;
-		ListNode l2 = mid.next;
-		mid.next = null;
-		l2 = reverseList(l2);
-		mergeList(l1, l2);
-	}
+    // 重排链表
+    public void reorderList(ListNode head) {
+        if (head == null) {
+            return;
+        }
+        ListNode mid = middleNode(head);
+        ListNode l1 = head;
+        ListNode l2 = mid.next;
+        mid.next = null;
+        l2 = reverseList(l2);
+        mergeList(l1, l2);
+    }
 
-	private void mergeList(ListNode l1, ListNode l2) {
-		ListNode l1_tmp;
-		ListNode l2_tmp;
-		while (l1 != null && l2 != null) {
-			l1_tmp = l1.next;
-			l2_tmp = l2.next;
-			l1.next = l2;
-			l1 = l1_tmp;
-			l2.next = l1;
-			l2 = l2_tmp;
-		}
-	}
+    private void mergeList(ListNode l1, ListNode l2) {
+        ListNode l1_tmp;
+        ListNode l2_tmp;
+        while (l1 != null && l2 != null) {
+            l1_tmp = l1.next;
+            l2_tmp = l2.next;
+            l1.next = l2;
+            l1 = l1_tmp;
+            l2.next = l1;
+            l2 = l2_tmp;
+        }
+    }
 
-	private ListNode reverseList(ListNode head) {
-		ListNode prev = null;
-		ListNode curr = head;
-		while (curr != null) {
-			ListNode next = curr.next;
-			curr.next = prev;
-			prev = curr;
-			curr = next;
-		}
-		return prev;
-	}
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
 
-	private ListNode middleNode(ListNode head) {
-		ListNode slow = head;
-		ListNode fast = head;
-		while (fast.next != null && fast.next.next != null) {
-			slow = slow.next;
-			fast = fast.next.next;
-		}
-		return slow;
-	}
+    private ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    // 下一个最大元素二
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        Arrays.fill(ans, -1);
+        Deque<Integer> deque = new ArrayDeque<>();
+        for (int i = 0; i < n * 2; i++) {
+            while (!deque.isEmpty() && nums[i % n] > nums[deque.peekLast()]) {
+                int poll = deque.pollLast();
+                ans[poll] = nums[i % n];
+            }
+            deque.addLast(i % n);
+        }
+        return ans;
+    }
 }
