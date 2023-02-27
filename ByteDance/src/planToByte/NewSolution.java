51b5ee5057208c4e51b6e7207bcf76a1ecb74368
kezi <wow.kimsohyun.wu@gamil.com>
2023-02-27 23:04:32 +0800

    2023-0227 daily

--- ByteDance/src/planToByte/NewSolution.java
+++ ByteDance/src/planToByte/NewSolution.java
@@ -265,4 +265,15 @@ public class NewSolution {
         }
         return ans;
     }
+
+	// 递减元素使数组呈锯齿状
+	public int movesToMakeZigzag(int[] nums) {
+		int[] s = new int[2];
+		for (int i = 0, n = nums.length; i < n; i++) {
+			int left = i > 0 ? nums[i - 1] : Integer.MAX_VALUE;
+			int right = i < n - 1 ? nums[i + 1] : Integer.MAX_VALUE;
+			s[i % 2] += Math.max(nums[i] - Math.min(left, right) + 1, 0);
+		}
+		return Math.min(s[0], s[1]);
+	}
 }
