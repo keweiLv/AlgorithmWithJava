eac21fc57e53d1d19a8311b29f8465065640854e
kezi <wow.kimsohyun.wu@gamil.com>
2023-03-04 21:10:58 +0800

    2023-0304 daily

--- ByteDance/src/planToByte/NewSolution.java
+++ ByteDance/src/planToByte/NewSolution.java
@@ -426,4 +426,36 @@ public class NewSolution {
 		}
 		return names;
 	}
+
+	// 寻找旋转数组中的最小值
+	public int findMin(int[] nums) {
+		int l = 0, r = nums.length - 1;
+		while (l < r) {
+			int mid = l + r >> 1;
+			if (nums[mid] > nums[nums.length - 1]) {
+				l = mid + 1;
+			} else {
+				r = mid;
+			}
+		}
+		return nums[l];
+	}
+
+	// 寻找旋转数组中的最小值Ⅱ
+	public int findMinTwo(int[] nums) {
+		int n = nums.length;
+		int l = 0, r = n - 1;
+		while (l < r && nums[0] == nums[r]) {
+			r--;
+		}
+		while (l < r) {
+			int mid = l + r + 1 >> 1;
+			if (nums[mid] >= nums[0]) {
+				l = mid;
+			} else {
+				r = mid - 1;
+			}
+		}
+		return r + 1 < n ? nums[r + 1] : nums[0];
+	}
 }
