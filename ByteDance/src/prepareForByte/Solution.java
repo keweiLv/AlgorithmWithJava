22f61870406d0aae96d1c461d47afd2e8e9c7ff6
kezi <wow.kimsohyun.wu@gamil.com>
2023-03-07 23:29:17 +0800

    2023-0307 daily

--- ByteDance/src/prepareForByte/Solution.java
+++ ByteDance/src/prepareForByte/Solution.java
@@ -403,4 +403,50 @@ public class Solution {
 		}
 		return ans;
 	}
+
+	// 非重叠矩形中的随机点
+	int[][] rs;
+	int[] sum;
+	int n;
+	Random random = new Random();
+
+	public Solution(int[][] rects) {
+		rs = rects;
+		n = rects.length;
+		sum = new int[n + 1];
+		for (int i = 1; i <= n; i++) {
+			sum[i] = sum[i - 1] + (rs[i - 1][2] - rs[i - 1][0] + 1) * (rs[i - 1][3] - rs[i - 1][1] + 1);
+		}
+	}
+
+	public int[] pick() {
+		int val = random.nextInt(sum[n]) + 1;
+		int l = 0, r = n;
+		while (l < r) {
+			int mid = l + r >> 1;
+			if (sum[mid] >= val) {
+				r = mid;
+			} else {
+				l = mid + 1;
+			}
+		}
+		int[] cur = rs[r - 1];
+		int x = random.nextInt(cur[2] - cur[0] + 1) + cur[0], y = random.nextInt(cur[3] - cur[1] + 1) + cur[1];
+		return new int[]{x, y};
+	}
+
+	// 和为K的子数组
+	public int subarraySum(int[] nums, int k) {
+		int count = 0, pre = 0;
+		Map<Integer, Integer> map = new HashMap<>();
+		map.put(0, 1);
+		for (int i = 0; i < nums.length; i++) {
+			pre += nums[i];
+			if (map.containsKey(pre - k)) {
+				count += map.get(pre - k);
+			}
+			map.put(pre, map.getOrDefault(pre, 0) + 1);
+		}
+		return count;
+	}
 }
