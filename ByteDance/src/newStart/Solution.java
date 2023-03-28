8d5f1dac43059d754a531873538dbe1038fc3c3a
kezi <wow.kimsohyun.wu@gamil.com>
2023-03-28 21:50:10 +0800

    2023-03-27 daily

--- ByteDance/src/newStart/Solution.java
+++ ByteDance/src/newStart/Solution.java
@@ -40,4 +40,96 @@ public class Solution {
         }
         return ans;
     }
+
+    // 采购方案
+    public int purchasePlans(int[] nums, int target) {
+        int mod = 1000000007;
+        int ans = 0;
+        Arrays.sort(nums);
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            if (nums[left] + nums[right] > target) {
+                right--;
+            } else {
+                ans += right - left;
+                left++;
+            }
+            ans %= mod;
+        }
+        return ans % mod;
+    }
+
+    // 重复叠加字符串匹配
+    public int repeatedStringMatch(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int ans = 0;
+        while (sb.length() < b.length() && ++ans > 0) {
+            sb.append(a);
+        }
+        sb.append(a);
+        int idx = sb.indexOf(b);
+        if(idx == -1){
+            return -1;
+        }
+        return idx + b.length() > a.length() * ans ? ans + 1 : ans;
+    }
+
+    // 找出字符串中第一个匹配项的下标
+    public int strStr(String haystack, String needle) {
+        int n = haystack.length(), m = needle.length();
+        char[] s = haystack.toCharArray(), p = needle.toCharArray();
+        for (int i = 0; i <= n - m; i++) {
+            int a = i, b = 0;
+            while (b < m && s[a] == p[b]) {
+                a++;
+                b++;
+            }
+            if (b == m) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // 统计只差一个字符的字串数目
+    public int countSubstrings(String s, String t) {
+        int ans = 0;
+        int m = s.length(),n = t.length();
+        for(int i = 0;;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(s.charAt(i) != t.charAt(j)){
+                    int l = 0,r = 0;
+                    while(i - l > 0 && j - l > 0 && s.charAt(i-l-1) == t.charAt(j-l-1)){
+                        ++l;
+                    }
+                    while(i + r + 1 < m && j + r + 1 <n && s.charAt(i + r +1) == t.charAt(j + r +1)){
+                        ++r;
+                    }
+                    ans += (1 + l) * (1 + r);
+                }
+            }
+        }
+        return ans;
+    }
+
+    // 最小展台数量
+    public int minNumBooths(String[] demand) {
+        int[] cnt = new int[26];
+        int[] cur = new int[26];
+        for (String s : demand) {
+            Arrays.fill(cur, 0);
+            for (int i = 0; i < s.length(); i++) {
+                int id = (int) s.charAt(i) - 'a';
+                cur[id]++;
+            }
+            for (int i = 0; i < 26; i++) {
+                cnt[i] = Math.max(cnt[i], cur[i]);
+            }
+        }
+        int ans = 0;
+        for (int num : cnt) {
+            ans += num;
+        }
+        return ans;
+    }
 }
