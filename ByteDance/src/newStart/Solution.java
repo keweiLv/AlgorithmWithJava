74ed533ce061feb645f55aa39572c1f31ffc7c21
kezi <wow.kimsohyun.wu@gmail.com>
2023-04-05 23:11:42 +0800

    2023-0405 daily

--- ByteDance/src/newStart/Solution.java
+++ ByteDance/src/newStart/Solution.java
@@ -389,4 +389,15 @@ public class Solution {
         return arr;
     }
 
+    // 共因子的数目
+    public int commonFactors(int a, int b) {
+        int ans = 0;
+        for (int i = 1;i<= Math.min(a,b);i++){
+            if (a % i ==0 && b % i == 0){
+                ans++;
+            }
+        }
+        return ans;
+    }
+
 }
