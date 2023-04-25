8bf667ea73234edfb58f6be3982be5a8d6b48661
kezi <wow.kimsohyun.wu@gmail.com>
2023-04-25 22:37:08 +0800

    2023-0425 daily

--- ByteDance/src/newStart/Solution.java
+++ ByteDance/src/newStart/Solution.java
@@ -1147,5 +1147,78 @@ public class Solution {
             end--;
         }
     }
+
+    // 等差数列中缺失的数字
+    public int missingNumber(int[] arr) {
+        int sum = Arrays.stream(arr).sum();
+        int n = arr.length;
+        int or = (arr[0] + arr[n - 1]) * (n + 1) / 2;
+        return or - sum;
+    }
+
+    // 按身高排序
+    public String[] sortPeople(String[] names, int[] heights) {
+        int n = names.length;
+        Integer[] idx = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            idx[i] = i;
+        }
+        Arrays.sort(idx, (i, j) -> heights[j] - heights[i]);
+        String[] ans = new String[n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = names[idx[i]];
+        }
+        return ans;
+    }
+
+    // 形成字符串的最短路径
+    public int shortestWay(String source, String target) {
+        int n = source.length();
+        int j = 0;
+        int ans = 0;
+        while (j < target.length()) {
+            int tmp = j;
+            for (int i = 0; i < n; i++) {
+                if (j < target.length() && source.charAt(i) == target.charAt(j)) {
+                    j++;
+                }
+            }
+            if (tmp == j) {
+                return -1;
+            }
+            ans++;
+        }
+        return ans;
+    }
+
+    // 粉刷房子
+    public int minCost(int[][] costs) {
+        int redCost = costs[0][0], blueCost = costs[0][1], greenCost = costs[0][2];
+        for (int i = 1; i < costs.length; i++) {
+            int newR = Math.min(blueCost, greenCost) + costs[i][0];
+            int newB = Math.min(redCost, greenCost) + costs[i][1];
+            int newG = Math.min(redCost, blueCost) + costs[i][2];
+            redCost = newR;
+            blueCost = newB;
+            greenCost = newG;
+        }
+        return Math.min(redCost, Math.min(blueCost, greenCost));
+    }
+
+    // 统计只含单一字母的子串
+    public int countLetters(String s) {
+        int ans = 0;
+        int count = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+            } else {
+                ans += count * (count + 1) / 2;
+                count = 1;
+            }
+        }
+        ans += count * (count + 1) / 2;
+        return ans;
+    }
 }
 
