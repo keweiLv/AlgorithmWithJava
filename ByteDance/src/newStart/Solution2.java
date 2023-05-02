38b986cf1fcab9c8635ca15982b65e9a6495300f
kezi <wow.kimsohyun.wu@gmail.com>
2023-05-02 23:51:35 +0800

    2023-0502 daily

--- ByteDance/src/newStart/Solution2.java
+++ ByteDance/src/newStart/Solution2.java
@@ -179,4 +179,21 @@ public class Solution2 {
         }
         return maxTime + informTime[id];
     }
+
+    // 强整数
+    public List<Integer> powerfulIntegers(int x, int y, int bound) {
+        Set<Integer> set = new HashSet<>();
+        for (int a = 1;a <= bound;a *= x){
+            for (int b = 1;a + b <= bound;b *= y){
+                set.add(a+b);
+                if (y == 1){
+                    break;
+                }
+            }
+            if (x == 1){
+                break;
+            }
+        }
+        return new ArrayList<>(set);
+    }
 }
