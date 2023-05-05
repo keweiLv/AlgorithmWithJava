fadcde80ed3006feb53401e59eb5b61018dc1d02
kezi <wow.kimsohyun.wu@gmail.com>
2023-05-06 00:06:25 +0800

    2023-0505 daily

--- ByteDance/src/newStart/Solution2.java
+++ ByteDance/src/newStart/Solution2.java
@@ -278,4 +278,18 @@ public class Solution2 {
         return ans;
     }
 
+    // 找出变味映射
+    public int[] anagramMappings(int[] nums1, int[] nums2) {
+        Map<Integer,Integer> D = new HashMap<>();
+        for (int i =0;i<nums2.length;i++){
+            D.put(nums2[i],i);
+        }
+        int[] ans = new int[nums1.length];
+        int t = 0;
+        for (int x:nums1){
+            ans[t++] = D.get(x);
+        }
+        return ans;
+    }
+
 }
