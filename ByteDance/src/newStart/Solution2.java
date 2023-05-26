f374529536be98363012b5968614b169b3c18511
kezi <wow.kimsohyun.wu@gmail.com>
2023-05-26 21:25:36 +0800

    2023-0526 daily

--- ByteDance/src/newStart/Solution2.java
+++ ByteDance/src/newStart/Solution2.java
@@ -1105,13 +1105,13 @@ public class Solution2 {
     // 删掉链表M个节点之后的N个节点
     public ListNode deleteNodes(ListNode head, int m, int n) {
         ListNode cur = head;
-        while (cur != null){
+        while (cur != null) {
             int num = 1;
-            while (num < m && cur != null){
+            while (num < m && cur != null) {
                 cur = cur.next;
                 num++;
             }
-            if (cur == null){
+            if (cur == null) {
                 return head;
             }
             num = 0;
@@ -1122,5 +1122,34 @@ public class Solution2 {
             cur = cur.next;
         }
         return head;
+    }
+
+    // 二进制矩阵中的最短路径
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        if (grid[0][0] == 1) {
+            return -1;
+        }
+        int n = grid.length;
+        grid[0][0] = 1;
+        Deque<int[]> q = new ArrayDeque<>();
+        q.offerLast(new int[]{0, 0});
+        for (int ans = 1; !q.isEmpty(); ans++) {
+            for (int k = q.size(); k > 0; k--) {
+                int[] poll = q.pollFirst();
+                int i = poll[0], j = poll[1];
+                if (i == n - 1 && j == n - 1) {
+                    return ans;
+                }
+                for (int x = i - 1; x <= i + 1; x++) {
+                    for (int y = j - 1; y <= j + 1; y++) {
+                        if (x >= 0 && x < n && y >= 0 && y < n && grid[x][y] == 0) {
+                            grid[x][y] = 1;
+                            q.offerLast(new int[]{x, y});
+                        }
+                    }
+                }
+            }
+        }
+        return -1;
     }
 }
