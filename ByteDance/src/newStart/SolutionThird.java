e22ad58b799e52084e20c33389f8ccb04a1891ca
kezi <wow.kimsohyun.wu@gmail.com>
2023-05-31 00:03:46 +0800

    2023-0530 daily

--- ByteDance/src/newStart/SolutionThird.java
+++ ByteDance/src/newStart/SolutionThird.java
@@ -1,7 +1,6 @@
 package newStart;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class SolutionThird {
 
@@ -37,5 +36,68 @@ public class SolutionThird {
         v = v - map.get(depth) + 1;
         dfs(root.left, v << 1, depth + 1);
         dfs(root.right, v << 1 | 1, depth + 1);
+    }
+
+    // 删点成林
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        List<TreeNode> ans = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+        for (int i : to_delete) {
+            set.add(i);
+        }
+        if (dfs(root, ans, set) != null) {
+            ans.add(root);
+        }
+        return ans;
+    }
+
+    private TreeNode dfs(TreeNode root, List<TreeNode> ans, Set<Integer> set) {
+        if (root == null) {
+            return null;
+        }
+        root.left = dfs(root.left, ans, set);
+        root.right = dfs(root.right, ans, set);
+        if (!set.contains(root.val)) {
+            return root;
+        }
+        if (root.left != null) {
+            ans.add(root.left);
+        }
+        if (root.right != null) {
+            ans.add(root.right);
+        }
+        return null;
+    }
+
+    // 数组中两个数的最大异或值
+    public int findMaximumXOR(int[] nums) {
+        int res = 0;
+        int mask = 0;
+        for (int i = 30; i >= 0; i--) {
+            mask = mask | (1 << i);
+            Set<Integer> set = new HashSet<>();
+            for (int num : nums) {
+                set.add(num & mask);
+            }
+            int temp = res | (1 << i);
+            for (Integer pre : set) {
+                if (set.contains(temp ^ pre)) {
+                    res = temp;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+
+    // 使用最小花费爬楼梯
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n + 1];
+        dp[0] = dp[1] = 0;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        }
+        return dp[n];
     }
 }
