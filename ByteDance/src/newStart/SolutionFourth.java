3b25f3bef41401e7b26e80e594a2fbe39d511107
kezi <wow.kimsohyun.wu@gmail.com>
2023-07-09 23:45:58 +0800

    2023-0709 daily

--- ByteDance/src/newStart/SolutionFourth.java
+++ ByteDance/src/newStart/SolutionFourth.java
@@ -752,4 +752,72 @@ public class SolutionFourth {
         }
         return res;
     }
+
+    // 三数之和
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        int len = nums.length;
+        if (nums == null || nums.length < 3) {
+            return res;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < len; i++) {
+            if (nums[i] > 0) {
+                break;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int l = i + 1;
+            int r = len - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum == 0) {
+                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    while (l < r && nums[l] == nums[l + 1]) {
+                        l++;
+                    }
+                    while (l < r && nums[r] == nums[r - 1]) {
+                        r--;
+                    }
+                    l++;
+                    r++;
+                } else if (sum < 0) {
+                    l++;
+                } else if (sum > 0) {
+                    r--;
+                }
+            }
+        }
+        return res;
+    }
+
+    // 二叉树剪枝
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        if (root.left == null && root.right == null && root.val == 0) {
+            return null;
+        }
+        return root;
+    }
+
+    // 山峰数组的顶部
+    public int peakIndexInMountainArray(int[] arr) {
+        int l = 1, r = arr.length - 2;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
+                return mid;
+            } else if (arr[mid] > arr[mid - 1]) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return l;
+    }
 }
