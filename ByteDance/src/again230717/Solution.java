13f8b8374ab9df0ec9e8fb7940a3ec80ba0791e3
kezi <wow.kimsohyun.wu@gmail.com>
2023-07-31 00:15:46 +0800

    2023-0730 daily

--- ByteDance/src/again230717/Solution.java
+++ ByteDance/src/again230717/Solution.java
@@ -262,4 +262,22 @@ public class Solution {
         }
         return f[n];
     }
+
+    // 零钱兑换
+    int INF = Integer.MAX_VALUE;
+
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        // 除以二是为了防止下面+1 后溢出
+        Arrays.fill(dp, INF/2);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        return dp[amount] == INF/2 ? -1 : dp[amount];
+    }
 }
