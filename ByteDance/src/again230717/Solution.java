94d891730505c5b01badf7f172dc283cf0a8afe0
kezi <wow.kimsohyun.wu@gmail.com>
2023-08-14 22:29:55 +0800

    2023-0814 daily

--- ByteDance/src/again230717/Solution.java
+++ ByteDance/src/again230717/Solution.java
@@ -825,4 +825,18 @@ public class Solution {
         return ans;
     }
 
+    // 合并二叉树
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) {
+            return null;
+        }
+        if (root1 == null || root2 == null) {
+            return root1 == null ? root2 : root1;
+        }
+        root1.val += root2.val;
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.right = mergeTrees(root1.right, root2.right);
+        return root1;
+    }
+
 }
