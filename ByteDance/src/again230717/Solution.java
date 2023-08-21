ef1533a0c0076a13ecccdc767cabd9829d2086e1
kezi <wow.kimsohyun.wu@gmail.com>
2023-08-21 23:58:33 +0800

    2023-0821 daily

--- ByteDance/src/again230717/Solution.java
+++ ByteDance/src/again230717/Solution.java
@@ -1004,4 +1004,41 @@ public class Solution {
         }
         return res == n + 1 ? 0 : res;
     }
+
+    //  移动片段得到字符串
+    public boolean canChange(String start, String target) {
+        if (!start.replaceAll("_", "").equals(target.replaceAll("_", ""))) {
+            return false;
+        }
+        for (int i = 0, j = 0; i < start.length(); i++) {
+            if (start.charAt(i) == '_') {
+                continue;
+            }
+            while (target.charAt(j) == '_') {
+                j++;
+            }
+            if (i != j && (start.charAt(i) == 'L') == (i < j)) {
+                return false;
+            }
+            j++;
+        }
+        return true;
+    }
+
+    // 优美的排列
+    public int[] constructArray(int n, int k) {
+        int[] ans = new int[n];
+        int t = n - k - 1;
+        for (int i = 0; i < t; i++) {
+            ans[i] = i + 1;
+        }
+        for (int i = t, a = n - k, b = n; i < n; ) {
+            ans[i++] = a++;
+            if (i < n) {
+                ans[i++] = b--;
+            }
+        }
+        return ans;
+    }
+
 }
