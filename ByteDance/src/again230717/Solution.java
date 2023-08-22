a6fe46bc414f086f7d055888ca6257b154dd14ee
kezi <wow.kimsohyun.wu@gmail.com>
2023-08-22 21:42:30 +0800

    2023-0822 daily

--- ByteDance/src/again230717/Solution.java
+++ ByteDance/src/again230717/Solution.java
@@ -1041,4 +1041,21 @@ public class Solution {
         return ans;
     }
 
+    // 离最近的人的最大距离
+    public int maxDistToClosest(int[] seats) {
+        int first = -1, last = -1;
+        int d = 0, n = seats.length;
+        for (int i = 0; i < n; i++) {
+            if (seats[i] == 1) {
+                if (last != -1) {
+                    d = Math.max(d, i - last);
+                }
+                if (first == -1) {
+                    first = i;
+                }
+                last = i;
+            }
+        }
+        return Math.max(d / 2, Math.max(first, n - 1 - last));
+    }
 }
