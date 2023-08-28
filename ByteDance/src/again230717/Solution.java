cbbbed3aa71489ccc0c39a97d39fcca48b30dce9
kezi <wow.kimsohyun.wu@gmail.com>
2023-08-28 22:57:53 +0800

    2023-0828 daily

--- ByteDance/src/again230717/Solution.java
+++ ByteDance/src/again230717/Solution.java
@@ -1233,5 +1233,25 @@ public class Solution {
         return Arrays.copyOf(ans, idx + 1);
     }
 
-
+    // 插入区间
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        ArrayList<int[]> res = new ArrayList<>();
+        int len = intervals.length;
+        int i = 0;
+        while (i < len && intervals[i][1] < newInterval[0]) {
+            res.add(intervals[i]);
+            i++;
+        }
+        while (i < len && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+            newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+            i++;
+        }
+        res.add(newInterval);
+        while (i < len && intervals[i][0] > newInterval[1]) {
+            res.add(intervals[i]);
+            i++;
+        }
+        return res.toArray(new int[0][]);
+    }
 }
