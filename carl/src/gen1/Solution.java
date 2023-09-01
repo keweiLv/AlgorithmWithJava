111777fa3f45458b05c9ec4faa1583cc1da19c69
kezi <wow.kimsohyun.wu@gmail.com>
2023-09-01 22:38:09 +0800

    2023-0901 daily

---
+++ carl/src/gen1/Solution.java
@@ -0,0 +1,37 @@
+package carl.src.gen1;
+
+/**
+ * @author kezi
+ */
+public class Solution {
+    // 二分查找
+    public int search(int[] nums, int target) {
+        if (target < nums[0] || target > nums[nums.length - 1]) {
+            return -1;
+        }
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) >> 1;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    // 移除元素
+    public int removeElement(int[] nums, int val) {
+        int slow = 0;
+        for (int fast = 0; fast < nums.length; fast++) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+        return slow;
+    }
+}
