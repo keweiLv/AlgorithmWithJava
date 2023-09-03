725bf2819ebc7e60493f604c4ab08d233709d2b0
kezi <wow.kimsohyun.wu@gmail.com>
2023-09-04 00:04:44 +0800

    2023-0903 daily

--- ByteDance/src/start230901/Solution.java
+++ ByteDance/src/start230901/Solution.java
@@ -1,5 +1,7 @@
 package start230901;
 
+import java.util.Arrays;
+
 public class Solution {
 
     // 买铅笔和钢笔的方案数
@@ -14,4 +16,21 @@ public class Solution {
         }
         return res;
     }
+
+    // 消灭怪物的最大数量
+    public int eliminateMaximum(int[] dist, int[] speed) {
+        int n = dist.length;
+        int[] arrivalTimes = new int[n];
+        for (int i = 0; i < n; i++) {
+            arrivalTimes[i] = (dist[i] - 1) / speed[i] + 1;
+        }
+        Arrays.sort(arrivalTimes);
+        for (int i = 0; i < n; i++) {
+            if (arrivalTimes[i] <= i) {
+                return i;
+            }
+        }
+        return n;
+    }
+
 }
