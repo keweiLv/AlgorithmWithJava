dfae6d63512f74e08aea731bf7f0de2e6e005589
kezi <wow.kimsohyun.wu@gmail.com>
2023-09-11 22:17:43 +0800

    2023-0911 daily

--- ByteDance/src/start230901/Solution.java
+++ ByteDance/src/start230901/Solution.java
@@ -214,6 +214,22 @@ public class Solution {
         }
         return ans;
     }
+
+    // 删除链表的倒数第 N 个节点
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode first = head;
+        ListNode second = dummy;
+        for (int i = 0; i < n; i++) {
+            first = first.next;
+        }
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    }
 }
 
 
