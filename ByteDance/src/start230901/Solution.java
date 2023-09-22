49e0a0341b9817fc93b6b3edaf99b54a75a22949
kezi <wow.kimsohyun.wu@gmail.com>
2023-09-23 00:07:09 +0800

    2023-0923 daily

--- ByteDance/src/start230901/Solution.java
+++ ByteDance/src/start230901/Solution.java
@@ -486,6 +486,20 @@ public class Solution {
         int right = maxDepth(root.right);
         return Math.max(left, right) + 1;
     }
+
+    // 将钱分给最多的儿童
+    public int distMoney(int money, int children) {
+        if (money < children) {
+            return -1;
+        }
+        if (money > 8 * children) {
+            return children - 1;
+        }
+        if (money == 8 * children - 4) {
+            return children - 2;
+        }
+        return (money - children) / 7;
+    }
 }
 
 
