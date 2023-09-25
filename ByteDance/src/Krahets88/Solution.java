56f448f4d3b15aa0f66d4d583423ff69fbb16633
kezi <wow.kimsohyun.wu@gmail.com>
2023-09-26 00:02:05 +0800

    2023-0925 daily

--- ByteDance/src/Krahets88/Solution.java
+++ ByteDance/src/Krahets88/Solution.java
@@ -17,5 +17,38 @@ public class Solution {
         }
     }
 
-    
+    // 反转链表
+    public ListNode reverseList(ListNode head) {
+        ListNode cur = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = cur;
+            cur = head;
+            head = next;
+        }
+        return cur;
+    }
+
+
+    /**
+     * 分割链表
+     * smlDummy 和 bigDummy 表示链表的头部，但它们不会改变，而 sml 和 big 在每次迭代中会移动到新的尾部节点，以确保将新的节点连接到正确的位置
+     */
+    public ListNode partition(ListNode head, int x) {
+        ListNode smlDummy = new ListNode(0), bigDummy = new ListNode(0);
+        ListNode sml = smlDummy, big = bigDummy;
+        while (head != null) {
+            if (head.val < x) {
+                sml.next = head;
+                sml = sml.next;
+            } else {
+                big.next = head;
+                big = big.next;
+            }
+            head = head.next;
+        }
+        sml.next = bigDummy.next;
+        big.next = null;
+        return smlDummy.next;
+    }
 }
