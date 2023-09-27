a731818ee8edd25077d8744e9c6772a65ff6d9f8
kezi <wow.kimsohyun.wu@gmail.com>
2023-09-28 00:04:00 +0800

    2023-0927 daily

--- ByteDance/src/Krahets88/Solution.java
+++ ByteDance/src/Krahets88/Solution.java
@@ -1,5 +1,9 @@
 package Krahets88;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
 public class Solution {
 
     // 合并两个有序链表
@@ -56,5 +60,62 @@ public class Solution {
     public void deleteNode(ListNode node) {
         node.val = node.next.val;
         node.next = node.next.next;
+    }
+
+    // 随机链表的复制
+    class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+        Node cur = head;
+        Map<Node, Node> map = new HashMap<>();
+        while (cur != null) {
+            map.put(cur, new Node(cur.val));
+            cur = cur.next;
+        }
+        cur = head;
+        while (cur != null) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+            cur = cur.next;
+        }
+        return map.get(head);
+    }
+
+    // 有效的括号
+    private static final Map<Character, Character> map = new HashMap<>() {{
+        put('{', '}');
+        put('[', ']');
+        put('(', ')');
+        put('?', '?');
+    }};
+
+    public boolean isValid(String s) {
+        if (s.length() > 0 && !map.containsKey(s.charAt(0))) {
+            return false;
+        }
+        LinkedList<Character> stack = new LinkedList<>() {{
+            add('?');
+        }};
+        for (Character c : s.toCharArray()) {
+            if (map.containsKey(c)) {
+                stack.addLast(c);
+            } else if (map.get(stack.removeLast()) != c) {
+                return false;
+            }
+        }
+        return stack.size() == 1;
     }
 }
--- ByteDance/src/start230901/Solution.java
+++ ByteDance/src/start230901/Solution.java
@@ -507,6 +507,18 @@ public class Solution {
         int mod = time % (n - 1);
         return (k & 1) == 1 ? n - mod : mod + 1;
     }
+
+    // 餐厅过滤器
+    public List<Integer> filterRestaurants(int[][] restaurants, int veganFriendly, int maxPrice, int maxDistance) {
+        Arrays.sort(restaurants, (a, b) -> a[1] == b[1] ? b[0] - a[0] : b[1] - a[1]);
+        List<Integer> ans = new ArrayList<>();
+        for (int[] r : restaurants) {
+            if (r[2] >= veganFriendly && r[3] <= maxPrice && r[4] <= maxDistance) {
+                ans.add(r[0]);
+            }
+        }
+        return ans;
+    }
 }
 
 
