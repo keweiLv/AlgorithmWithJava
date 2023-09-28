b911b1ff92cdf26e94fa9f46915c84966b333d0f
kezi <wow.kimsohyun.wu@gmail.com>
2023-09-28 23:40:50 +0800

    2023-0928 daily

---
+++ ByteDance/src/Krahets88/MinStack.java
@@ -0,0 +1,35 @@
+package Krahets88;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class MinStack {
+
+    Deque<Integer> xStack;
+    Deque<Integer> minStack;
+
+
+    public MinStack() {
+        xStack = new LinkedList<>();
+        minStack = new LinkedList<>();
+        minStack.push(Integer.MAX_VALUE);
+    }
+
+    public void push(int val) {
+        xStack.push(val);
+        minStack.push(Math.min(minStack.peek(), val));
+    }
+
+    public void pop() {
+        xStack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return xStack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
