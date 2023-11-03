e5aa28f7f161252585b86422d97de8b582511d23
kezi <wow.kimsohyun.wu@gmail.com>
2023-11-03 12:34:53 +0800

    2023-1103 daily

--- ByteDance/src/November2023/Solution.java
+++ ByteDance/src/November2023/Solution.java
@@ -1,5 +1,8 @@
 package November2023;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Solution {
 
     // 水果成篮
@@ -42,6 +45,83 @@ public class Solution {
                 left++;
             }
             ans += right - left;
+        }
+        return ans;
+    }
+
+    // 填充每个节点的下一个右侧节点指针二
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+
+    ;
+
+    public Node connect(Node root) {
+        Node res = root;
+        if (root == null) {
+            return res;
+        }
+        Deque<Node> deque = new ArrayDeque<>();
+        deque.addLast(root);
+        while (!deque.isEmpty()) {
+            int sz = deque.size();
+            while (sz-- > 0) {
+                Node cur = deque.pollFirst();
+                if (sz != 0) {
+                    cur.next = deque.peekFirst();
+                }
+                if (cur.left != null) {
+                    deque.addLast(cur.left);
+                }
+                if (cur.right != null) {
+                    deque.addLast(cur.right);
+                }
+            }
+        }
+        return res;
+    }
+
+    // 分隔链表
+    public ListNode[] splitListToParts(ListNode head, int k) {
+        int cnt = 0;
+        ListNode temp = head;
+        while (temp != null && ++cnt > 0) {
+            temp = temp.next;
+        }
+        int pre = cnt / k;
+        ListNode[] ans = new ListNode[pre];
+        for (int i = 0, sum = 1; i < k; i++, sum++) {
+            ans[i] = head;
+            temp = ans[i];
+            int u = pre;
+            while (u-- > 0 && ++sum > 0) {
+                temp = temp.next;
+            }
+            int remain = k - i - 1;
+            if (pre != 0 && sum + pre * remain < cnt && ++sum > 0) {
+                temp = temp.next;
+            }
+            head = temp != null ? temp.next : null;
+            if (temp != null) {
+                temp.next = null;
+            }
         }
         return ans;
     }
