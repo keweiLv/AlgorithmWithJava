f250d4fcdb6996f8f2e399e30c953c0a1a557923
kezi <wow.kimsohyun.wu@gmail.com>
2023-11-24 01:01:23 +0800

    2023-1123 daily

--- ByteDance/src/Krahets88/Solution.java
+++ ByteDance/src/Krahets88/Solution.java
@@ -259,4 +259,18 @@ public class Solution {
         return sb.toString();
     }
 
+    // 无重复字符的最长子串
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int max = 0, left = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                left = Math.max(left, map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i), i);
+            max = Math.max(max, i - left + 1);
+        }
+        return max;
+    }
+
 }
