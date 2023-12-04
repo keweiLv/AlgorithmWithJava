230a04f98d1f6670e4cd4e6d86b1750d0ef90a8e
kezi <wow.kimsohyun.wu@gmail.com>
2023-12-04 23:34:47 +0800

    2023-1204 daily

--- ByteDance/src/November2023/Solution.java
+++ ByteDance/src/November2023/Solution.java
@@ -371,4 +371,77 @@ public class Solution {
         }
         return sb.toString();
     }
+
+    // 从二叉搜索树到更大和树
+    private int s = 0;
+
+    public TreeNode bstToGst(TreeNode root) {
+        dfs(root);
+        return root;
+    }
+
+    private void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.right);
+        s += root.val;
+        root.val = s;
+        dfs(root.left);
+    }
+
+    // 蜡烛之间的盘子
+    public int[] platesBetweenCandles(String s, int[][] queries) {
+        char[] cs = s.toCharArray();
+        int n = cs.length, m = queries.length;
+        int[] ans = new int[m], sum = new int[n + 1];
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (cs[i] == '|') {
+                list.add(i);
+            }
+            sum[i + 1] = sum[i] + (cs[i] == '*' ? 1 : 0);
+        }
+        if (list.size() < 2) {
+            return ans;
+        }
+        for (int i = 0; i < m; i++) {
+            int a = queries[i][0], b = queries[i][1];
+            int c = -1, d = -1;
+            int l = 0, r = list.size() - 1;
+            while (l < r) {
+                int mid = l + r >> 1;
+                if (list.get(mid) > a) {
+                    r = mid;
+                } else {
+                    l = mid + 1;
+                }
+            }
+            if (list.get(r) >= a) {
+                c = list.get(r);
+            } else {
+                continue;
+            }
+            l = 0;
+            r = list.size() - 1;
+            while (l < r) {
+                int mid = l + r + 1 >> 1;
+                if (list.get(mid) < b) {
+                    l = mid;
+                } else {
+                    r = mid - 1;
+                }
+            }
+            if (list.get(r) <= b) {
+                d = list.get(r);
+            } else {
+                continue;
+            }
+            if (c <= d) {
+                ans[i] = sum[d + 1] - sum[c];
+            }
+        }
+        return ans;
+    }
+
 }
