d234408b704bac60ace1e3c61267d1adca6edff4
kezi <wow.kimsohyun.wu@gmail.com>
2023-12-13 22:18:14 +0800

    2023-1213 daily

--- ByteDance/src/November2023/Solution.java
+++ ByteDance/src/November2023/Solution.java
@@ -610,4 +610,32 @@ public class Solution {
         }
         return deque.peek();
     }
+
+    // 下一个更大元素四
+    public int[] secondGreaterElement(int[] nums) {
+        int[] res = new int[nums.length];
+        Arrays.fill(res, -1);
+        Deque<Integer> stack = new ArrayDeque<Integer>();
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> a[0] - b[0]);
+        for (int i = 0; i < nums.length; ++i) {
+            while (!pq.isEmpty() && pq.peek()[0] < nums[i]) {
+                res[pq.poll()[1]] = nums[i];
+            }
+            while (!stack.isEmpty() && nums[stack.peek()] < nums[i]) {
+                pq.offer(new int[]{nums[stack.peek()], stack.peek()});
+                stack.pop();
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+
+    // 字典序最小回文串
+    public String makeSmallestPalindrome(String s) {
+        char[] cs = s.toCharArray();
+        for (int i = 0, j = cs.length - 1; i < j; i++, j--) {
+            cs[i] = cs[j] = (char) Math.min(cs[i], cs[j]);
+        }
+        return new String(cs);
+    }
 }
