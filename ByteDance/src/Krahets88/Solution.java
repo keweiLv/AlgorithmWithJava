6cfd4af5eafb3ff13c326f2dcdce1aa0165dfbd3
kezi <wow.kimsohyun.wu@gmail.com>
2024-01-04 22:16:01 +0800

    2024-0104 daily

--- ByteDance/src/Krahets88/Solution.java
+++ ByteDance/src/Krahets88/Solution.java
@@ -439,4 +439,20 @@ public class Solution {
         return false;
     }
 
+
+    // 二叉树的最近公共祖先
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null) {
+            return right;
+        }
+        if (right == null) {
+            return left;
+        }
+        return root;
+    }
 }
