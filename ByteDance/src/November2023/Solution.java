66e06135b695e37f89c901c86e276cf92ad62b8d
kezi <wow.kimsohyun.wu@gmail.com>
2024-01-23 22:18:17 +0800

    2024-0123 daily

--- ByteDance/src/November2023/Solution.java
+++ ByteDance/src/November2023/Solution.java
@@ -1413,4 +1413,22 @@ public class Solution {
         return Integer.parseInt(new String(s));
     }
 
+    // 最长交替子数组
+    public int alternatingSubarray(int[] nums) {
+        int ans = -1;
+        int i = 0, n = nums.length;
+        while (i < n - 1) {
+            if (nums[i + 1] - nums[i] != 1) {
+                continue;
+            }
+            int i0 = i;
+            i += 2;
+            while (i < n && nums[i] == nums[i - 2]) {
+                i++;
+            }
+            ans = Math.max(ans, i - i0);
+            i--;
+        }
+        return ans;
+    }
 }
