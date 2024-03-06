42d79b26184d5b97b69e43c40d0344e24a6a9933
kezi <wow.kimsohyun.wu@gmail.com>
2024-03-06 09:12:15 +0800

    2024-0305 daily

--- ByteDance/src/start240304/Solution.java
+++ ByteDance/src/start240304/Solution.java
@@ -1,4 +1,37 @@
 package start240304;
 
+import java.util.List;
+
 public class Solution {
+
+    // 最短单词距离
+    public int shortestDistance(String[] wordsDict, String word1, String word2) {
+        int len = wordsDict.length;
+        int ans = len;
+        int idx1 = -1, idx2 = -1;
+        for (int i = 0; i < len; i++) {
+            String word = wordsDict[i];
+            if (word.equals(word1)) {
+                idx1 = i;
+            } else if (word.equals(word2)) {
+                idx2 = i;
+            }
+            if (idx1 >= 0 && idx2 >= 0) {
+                ans = Math.min(ans, Math.abs(idx1 - idx2));
+            }
+        }
+        return ans;
+    }
+
+    // 三角形最小路径和
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[] dp = new int[n + 1];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[j] = Math.min(dp[j], dp[j + 1]) + triangle.get(i).get(j);
+            }
+        }
+        return dp[0];
+    }
 }
