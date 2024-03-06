b777f05f10b26e2a082a7b1e29665e9427d19a41
kezi <wow.kimsohyun.wu@gmail.com>
2024-03-06 23:19:06 +0800

    2024-0306 daily

--- ByteDance/src/start240304/Solution.java
+++ ByteDance/src/start240304/Solution.java
@@ -1,6 +1,8 @@
 package start240304;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Solution {
 
@@ -33,5 +35,19 @@ public class Solution {
             }
         }
         return dp[0];
+    }
+
+    // 无重复字符的最长子串
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int max = 0, left = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                left = Math.max(left, map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i), i);
+            max = Math.max(max, i - left + 1);
+        }
+        return max;
     }
 }
