b04240ec0a1c5c0212729512d2091fb72f074644
kezi <wow.kimsohyun.wu@gmail.com>
2024-03-08 22:35:23 +0800

    2024-0308 daily

--- ByteDance/src/start240304/Solution.java
+++ ByteDance/src/start240304/Solution.java
@@ -50,4 +50,15 @@ public class Solution {
         }
         return max;
     }
+
+    // 找出美丽数组的最小和
+    final int MOD = (int) 1e9 + 7;
+
+    public int minimumPossibleSum(int n, int target) {
+        int m = target / 2;
+        if (n <= m) {
+            return (int) ((long) (1 + n) * n / 2 % MOD);
+        }
+        return (int) (((long) (1 + m) * m / 2 + ((long) target + target + (n - m) - 1) * (n - m) / 2) % MOD);
+    }
 }
