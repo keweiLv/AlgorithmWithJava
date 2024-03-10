fd33bf1250f89ad9b73619e5a42a54842cbaa6fd
kezi <wow.kimsohyun.wu@gmail.com>
2024-03-10 23:19:30 +0800

    2024-0310 daily

--- ByteDance/src/start240304/Solution.java
+++ ByteDance/src/start240304/Solution.java
@@ -61,4 +61,24 @@ public class Solution {
         }
         return (int) (((long) (1 + m) * m / 2 + ((long) target + target + (n - m) - 1) * (n - m) / 2) % MOD);
     }
+
+    // 猜数字游戏
+    public String getHint(String secret, String guess) {
+        int bulls = 0;
+        int[] cntS = new int[10];
+        int[] cntG = new int[10];
+        for (int i = 0; i < guess.length(); i++) {
+            if (secret.charAt(i) == guess.charAt(i)) {
+                bulls++;
+            } else {
+                cntS[secret.charAt(i) - '0']++;
+                cntG[guess.charAt(i) - '0']++;
+            }
+        }
+        int cows = 0;
+        for (int i = 0; i < 10; i++) {
+            cows += Math.min(cntS[i], cntG[i]);
+        }
+        return Integer.toString(bulls) + "A" + Integer.toString(cows) + "B";
+    }
 }
