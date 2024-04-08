0abafd6f18419fbb19db03c5f765f54930f63c2e
kezi <wow.kimsohyun.wu@gmail.com>
2024-04-08 22:40:18 +0800

    2024-0408 daily

--- ByteDance/src/start240304/Solution.java
+++ ByteDance/src/start240304/Solution.java
@@ -401,5 +401,38 @@ public class Solution {
         }
         return a;
     }
+
+    // 删除有序数组中的重复项
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        int m = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[m++] = nums[i];
+            }
+        }
+        return m;
+    }
+
+    // 使数组连续的最少操作数
+    public int minOperations(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int m = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[m++] = nums[i];
+            }
+        }
+        int ans = 0;
+        int left = 0;
+        for (int i = 0; i < m; i++) {
+            while (nums[left] < nums[i] - n + 1) {
+                left++;
+            }
+            ans = Math.max(ans, i - left + 1);
+        }
+        return n - ans;
+    }
 }
 
