0e661c3e71fd41b174136630ddc7e20e075cc35d
kezi <wow.kimsohyun.wu@gmail.com>
2024-04-09 21:54:01 +0800

    2024-0409 daily

--- ByteDance/src/start240304/Solution.java
+++ ByteDance/src/start240304/Solution.java
@@ -434,5 +434,38 @@ public class Solution {
         }
         return n - ans;
     }
+
+    // 正整数和负整数的最大计数
+    public int maximumCount(int[] nums) {
+        int m = 0, n = 0;
+        m = (int) Arrays.stream(nums).filter(x -> x < 0).count();
+        n = (int) Arrays.stream(nums).filter(x -> x > 0).count();
+        return Math.max(m, n);
+    }
+
+    // 字符串转换整数
+    public int myAtoi(String s) {
+        char[] c = s.trim().toCharArray();
+        if (c.length == 0) {
+            return 0;
+        }
+        int res = 0, board = Integer.MAX_VALUE / 10;
+        int i = 1, sign = 1;
+        if (c[0] == '-') {
+            sign = -1;
+        } else if (c[0] != '+') {
+            i = 0;
+        }
+        for (int j = i; j < c.length; j++) {
+            if (c[j] < '0' || c[j] > '9') {
+                break;
+            }
+            if (res > board || res == board && c[j] > '7') {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            res = res * 10 + (c[j] - '0');
+        }
+        return sign * res;
+    }
 }
 
