0884a7c7c7f80b0fb0550959b5d2e5d127c80957
kezi <wow.kimsohyun.wu@gmail.com>
2024-04-11 21:47:28 +0800

    2024-0411 daily

--- ByteDance/src/start240304/Solution.java
+++ ByteDance/src/start240304/Solution.java
@@ -502,5 +502,34 @@ public class Solution {
         }
         return res;
     }
+
+    // 全排列二
+    boolean[] vis;
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        vis = new boolean[nums.length];
+        Arrays.sort(nums);
+        backtrace(nums, ans, 0, path);
+        return ans;
+    }
+
+    private void backtrace(int[] nums, List<List<Integer>> ans, int idx, List<Integer> path) {
+        if (idx == nums.length) {
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (vis[i] || (i > 0 && nums[i] == nums[i - 1] && !vis[i - 1])) {
+                continue;
+            }
+            path.add(nums[i]);
+            vis[i] = true;
+            backtrace(nums, ans, idx + 1, path);
+            vis[i] = false;
+            path.remove(path.size() - 1);
+        }
+    }
 }
 
