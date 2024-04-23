bb08a76677be843b94c7f89f67571b94158c2a0c
kezi <wow.kimsohyun.wu@gmail.com>
2024-04-23 23:32:21 +0800

    2024-0423 daily

--- ByteDance/src/start240304/Solution.java
+++ ByteDance/src/start240304/Solution.java
@@ -720,5 +720,31 @@ public class Solution {
             path.remove(path.size() - 1);
         }
     }
+
+    // 爱生气的书店老板
+    public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
+        int n = customers.length, ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (grumpy[i] == 0) {
+                ans += customers[i];
+                customers[i] = 0;
+            }
+        }
+        int cur = 0, max = 0;
+        for (int i = 0; i < n; i++) {
+            cur += customers[i];
+            if (i >= minutes) {
+                cur -= customers[i - minutes];
+            }
+            max = Math.max(max, cur);
+        }
+        return ans + max;
+    }
+
+    // 第N位数字
+    public int findNthDigit(int n) {
+
+    }
+
 }
 
