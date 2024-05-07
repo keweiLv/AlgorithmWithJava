83a33eec36947d206d098eadf19a713578d65d4b
kezi <wow.kimsohyun.wu@gmail.com>
2024-05-07 23:57:43 +0800

    2024-0507 daily

--- ByteDance/src/start240304/Solution.java
+++ ByteDance/src/start240304/Solution.java
@@ -860,5 +860,31 @@ public class Solution {
     private boolean inArea(int newX, int newY) {
         return newX >= 0 && newX < rows && newY >= 0 && newY < cols;
     }
+
+    // 从前序与中序遍历序列构造二叉树
+    private Map<Integer, Integer> indexMap;
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n = preorder.length;
+        indexMap = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            indexMap.put(inorder[i], i);
+        }
+        return myBuildTree(preorder, inorder, 0, n - 1, 0, n - 1);
+    }
+
+    private TreeNode myBuildTree(int[] preorder, int[] inorder, int preorder_left, int preorder_right, int inorder_left, int inorder_right) {
+        if (preorder_left > preorder_right) {
+            return null;
+        }
+        int preorder_root = preorder_left;
+        int inorder_root = indexMap.get(preorder[preorder_root]);
+
+        TreeNode root = new TreeNode(preorder[preorder_root]);
+        int left_size = inorder_root - inorder_left;
+        root.left = myBuildTree(preorder, inorder, preorder_left + 1, preorder_left + left_size, inorder_left, inorder_root - 1);
+        root.right = myBuildTree(preorder, inorder, preorder_left + left_size + 1, preorder_right, inorder_root + 1, inorder_right);
+        return root;
+    }
 }
 
