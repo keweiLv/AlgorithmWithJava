f38c8562da65c6998b938734a7711a0f284f9585
kezi <wow.kimsohyun.wu@gmail.com>
2024-05-22 00:01:29 +0800

    2024-0521 daily

--- ByteDance/src/start240304/Solution.java
+++ ByteDance/src/start240304/Solution.java
@@ -1030,5 +1030,25 @@ public class Solution {
         }
         return ans;
     }
+
+    // 除自身以外数组的乘积
+    public int[] productExceptSelf(int[] nums) {
+        int len = nums.length;
+        int[] L = new int[len];
+        int[] R = new int[len];
+        int[] ans = new int[len];
+        L[0] = 1;
+        for (int i = 1; i < len; i++) {
+            L[i] = L[i - 1] * nums[i - 1];
+        }
+        R[len - 1] = 1;
+        for (int i = len - 2; i >= 0; i--) {
+            R[i] = R[i + 1] * nums[i + 1];
+        }
+        for (int i = 0; i < len; i++) {
+            ans[i] = L[i] * R[i];
+        }
+        return ans;
+    }
 }
 
