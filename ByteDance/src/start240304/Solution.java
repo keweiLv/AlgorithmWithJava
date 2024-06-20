567661c7198ff587782d45dc33d32631e7649d6d
kezi <wow.kimsohyun.wu@gmail.com>
2024-06-20 18:01:42 +0800

    2024-0620 daily

--- ByteDance/src/start240304/Solution.java
+++ ByteDance/src/start240304/Solution.java
@@ -1165,4 +1165,26 @@ public class Solution {
         }
         return res;
     }
+
+    // 美丽下标对的数目
+    public int countBeautifulPairs(int[] nums) {
+        int ans = 0;
+        int[] cnt = new int[10];
+        for (int x : nums) {
+            for (int y = 1; y < 10; y++) {
+                if (cnt[y] > 0 && gcd(y,x % 10) == 1){
+                    ans += cnt[y];
+                }
+            }
+            while (x >= 10){
+                x /= 10;
+            }
+            cnt[x]++;
+        }
+        return ans;
+    }
+
+    private int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
 }
