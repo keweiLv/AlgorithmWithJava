acc112d0d35f348cb46a05aa9280f1ba2dbf324f
kezi <wow.kimsohyun.wu@gmail.com>
2024-06-26 20:28:43 +0800

    2024-0626 daily

--- ByteDance/src/start240304/Solution.java
+++ ByteDance/src/start240304/Solution.java
@@ -1211,4 +1211,19 @@ public class Solution {
         }
         return ans + 1;
     }
+
+    // 格雷编码
+    public List<Integer> grayCode(int n) {
+        List<Integer> res = new ArrayList<>() {{
+            add(0);
+        }};
+        int head = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = res.size() - 1; j >= 0; j--) {
+                res.add(head + res.get(j));
+            }
+            head <<= 1;
+        }
+        return res;
+    }
 }
