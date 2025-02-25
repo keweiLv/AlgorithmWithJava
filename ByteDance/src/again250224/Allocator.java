38ecae68e01ef4f9187479f39638094a6461ffe5
kezi <wow.kimsohyun.wu@gamil.com>
2025-02-25 23:07:22 +0800

    2025-0225 daily

---
+++ ByteDance/src/again250224/Allocator.java
@@ -0,0 +1,20 @@
+
+
+/**
+ * 设计内存分配器
+ */
+public class Allocator {
+
+    public Allocator(int n) {
+        
+    }
+    
+    public int allocate(int size, int mID) {
+        
+    }
+    
+    public int freeMemory(int mID) {
+        
+    }
+    
+}
